fd62c890a228c5c7aa209068bc7a6db0ee06a11f
sgbaird <45469701+sgbaird@users.noreply.github.com>
2020-07-23 22:14:27 -0600

    get_omega function validation
    Using arguments ... end syntax (introduced in R2019b). If conflict, ok to comment from "arguments" to the corresponding "end". Defined and tested custom validation function for the norms.

--- code/get_omega.m
+++ code/get_omega.m
@@ -1,4 +1,8 @@
 function omega = get_omega(o1,o2)
+arguments
+	o1 (:,8) double {normMustBeSqrt2(o1),quatNormsMustBeOne(o1)}
+	o2 (:,8) double {normMustBeSqrt2(o2),quatNormsMustBeOne(o2)}
+end
 %--------------------------------------------------------------------------
 % Author: Sterling Baird
 %
@@ -25,3 +29,25 @@ omega = 2*acos(abs(dot1+dot2)/2);
 
 end
 %----------------------------END get_omega()-------------------------------
+
+
+%--------------------------VALIDATION FUNCTIONS----------------------------
+% Custom validation function
+function normMustBeSqrt2(val)
+
+tol = 0.1;
+normcheck = abs(norm(val(1,:))-sqrt(2)) < tol;
+errmsg = ['octonion norm must be equal to sqrt(2) within tol = ' num2str(tol)];
+assert(normcheck,errmsg)
+
+end
+
+function quatNormsMustBeOne(val)
+
+tol = 0.1;
+qAcheck = abs(norm(val(1,1:4))-1) < tol;
+qBcheck = abs(norm(val(1,5:8))-1) < tol;
+errmsg = ['quaternion norms must be equal to 1 within tol = ' num2str(tol)];
+assert(qAcheck && qBcheck,errmsg)
+
+end
