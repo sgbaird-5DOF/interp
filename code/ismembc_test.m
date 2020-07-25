ecfe8ad2ce7197608a3de2b3cf425161ecfe7860
sgbaird <45469701+sgbaird@users.noreply.github.com>
2020-07-24 18:16:30 -0600

    Create ismembc_test.m
    speed function

---
+++ code/ismembc_test.m
@@ -0,0 +1,19 @@
+%ismembc vs. ismember test
+
+disp('long lists of numbers')
+n=7*1e5; a=ceil(n*rand(n,1)); b=ceil(n*rand(n,1));
+
+t1a = timeit(@() ismembc(a,b))
+t1b = timeit(@() ismember(a,b))
+
+keyboard
+
+disp('sorted long lists of numbers')
+a = reshape(a,7,[]);
+b = reshape(b,7,[]);
+
+t2a = timeit(@() ismembc(a,b))
+t2b = timeit(@() ismember(a,b))
+
+disp('sparse array and set')
+A = randi(floor(1e6/7),1e6,7);
