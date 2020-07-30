972a92bcde1c5854374dac058c175a44bbc9bad7
sgbaird <45469701+sgbaird@users.noreply.github.com>
2020-07-30 16:37:09 -0600

    added another case

--- code/hsphext_subdiv_test.m
+++ code/hsphext_subdiv_test.m
@@ -76,4 +76,28 @@ switch test
 			axis equal tight
 			
 		end
+		
+	case 3
+		%probing a convex hull
+		seed = 10;
+		rng(seed);
+		
+		d = 3;
+		
+		%mesh
+		nmeshpts = 100;
+		orthoPts = orthoplex(d);
+		ids = find(all(orthoPts >= 0, 2));
+		orthoPts = orthoPts(ids,:);
+		meshpts = normr(rand(nmeshpts,d));
+		[~,K,pts] = hsphext_subdiv(meshpts);
+		% meshprops = (1:nmeshpts)/nmeshpts*10 + rand(1,nmeshpts);
+		
+		randpoly = rand(d,1);
+		meshprops = meshpts*randpoly;
+		
+		%data
+		ndatapts = 10;
+		datapts = normr(rand(ndatapts,d));
+		dataprops = datapts*randpoly;
 end
