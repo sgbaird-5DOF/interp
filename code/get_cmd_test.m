d83cfe9bf004d35dbfd88495098b61ca35da1626
Sterling <sterling.baird@icloud.com>
2020-08-13 16:25:26 -0600

    data transfer, parameter changes

--- code/get_cmd_test.m
+++ code/get_cmd_test.m
@@ -2,10 +2,10 @@
 %N_trim = [10 20 15 11];
 N_trim = [1];
 jid = 1;
-cores = 24;
+cores = 8;
 setenv('cores',int2str(cores))
-walltimes = repelem(59,length(N_trim));
-mem = 1024*4; %MB
+walltimes = repelem(60,length(N_trim));
+mem = 1024*6; %MB
 qosopt = 'test'; %'', 'test'
 script_fpath = 'submit.sh';
 dirpath = './';
