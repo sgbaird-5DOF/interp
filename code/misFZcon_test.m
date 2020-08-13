b3c21af39170489dc211e1b47611614a08328667
sgbaird <45469701+sgbaird@users.noreply.github.com>
2020-08-13 17:16:24 -0600

    faster helper function (replaced ismembertol)

--- code/misFZcon_test.m
+++ code/misFZcon_test.m
@@ -2,11 +2,18 @@
 clear; close all
 [A,b] = misFZcon();
 
-pts = cprnd(10000,A,b);
+dlist = cprnd(10000,A,b);
 
 plotFZrodriguez_vtx();
-t=n2c(pts);
+t=n2c(dlist);
 plot3(t{:},'*')
 
+qlist = disorientation(rod2q(dlist),'cubic');
 
-misFZ = inmisFZ(dlist)
+geometry = findgeometry(qlist,1e-2);
+sum(~ismember(geometry,{'interior','exterior'}))
+dlist = q2rod(qlist);
+
+misFZ = inmisFZ(dlist);
+
+sum(misFZ)
