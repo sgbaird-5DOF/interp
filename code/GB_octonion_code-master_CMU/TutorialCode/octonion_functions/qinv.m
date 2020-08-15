e4cd0b8a8398bef0a6fdaa2440fcfb453dbf4615
sgbaird <45469701+sgbaird@users.noreply.github.com>
2020-08-15 03:06:27 -0600

    vectorized

--- code/GB_octonion_code-master_CMU/TutorialCode/octonion_functions/qinv.m
+++ code/GB_octonion_code-master_CMU/TutorialCode/octonion_functions/qinv.m
@@ -1,10 +1,12 @@
 function out = qinv(q)
+%vectorized by SGB 2020-08-15
 %%% Lp(r) = vec(prp*) active. For passive, implement Lp*(r) = vec(p*rp)
 
-q0 = q(1);
-q = q(2:4);
+q0 = q(:,1);
+q = q(:,2:4);
 out1 = [q0 -q];
-out = out1/norm(out1);
+out = normr(out1);
+% out = out1./vecnorm(out1,2);
 
 % qr = pp(1)*qq(1)-dot(p,q);
 % qi = pp(1)*q + qq(1)*p + cross(p,q);
