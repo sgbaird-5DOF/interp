a44950c1097599c5eacfd82f1492c5f96a9199be
sgbaird <45469701+sgbaird@users.noreply.github.com>
2020-08-15 01:28:28 -0600

    vectorized

--- code/GB_octonion_code-master_CMU/TutorialCode/rotation_conversions/eu2qu.m
+++ code/GB_octonion_code-master_CMU/TutorialCode/rotation_conversions/eu2qu.m
@@ -1,34 +1,70 @@
-% from Euler angles to quaternions
-
 function q = eu2qu(eu)
-
+%--------------------------------------------------------------------------
+% Date:
+%
+% Description:  from Euler angles to quaternions
+% 
+% Inputs:
+%  a - a
+%
+% Outputs:
+%  b - b
+%
+% Usage:
+%  a = b(a);
+%
+% Dependencies:
+%  *
+%
+% Notes:
+%  Vectorized by SGB 2020-08-15
+%--------------------------------------------------------------------------
 global epsijk
-% epsijk = -1;
 epsijk
+if isempty(epsijk)
+	epsijk = -1;
+	warning(['global variable epsijk not set. Using Bunge convention: epsijk == ' int2str(epsijk)])
+end
 
-% q = zeros(1,4);
-
-aphi1 = eu(1);
-aPhi = eu(2);
-aphi2 = eu(3);
+aphi1 = eu(:,1);
+aPhi = eu(:,2);
+aphi2 = eu(:,3);
 
 sigma = 0.5*(aphi1+aphi2);
 delta = 0.5*(aphi1-aphi2);
 c = cos(aPhi/2);
 s = sin(aPhi/2);
 
-q0 = c*cos(sigma);
+q = [c*cos(sigma); -epsijk*s*cos(delta); -epsijk*s*sin(delta); -epsijk*c*sin(sigma)];
 
-% q = [c*cos(sigma); -epsijk*s*cos(delta); -epsijk*s*sin(delta); -epsijk*c*sin(sigma)];
-
-if q0>=0
-    q = [c*cos(sigma), -epsijk*s*cos(delta), -epsijk*s*sin(delta), -epsijk*c*sin(sigma)];
-else
-    q = [-c*cos(sigma), epsijk*s*cos(delta), epsijk*s*sin(delta), epsijk*c*sin(sigma)];
-end
+ids = q(:,1) >= 0;
+q(ids,:) = -q(ids,:);
 
 % set values very close to 0 as 0
 thr = 1e-10;
+
+q(abs(q)<thr) = 0;
+
+end
+
+%--------------------------------CODE GRAVEYARD----------------------------
+%{
+% epsijk = -1;
+
+% q = zeros(1,4);
+
+% q = [c*cos(sigma); -epsijk*s*cos(delta); -epsijk*s*sin(delta); -epsijk*c*sin(sigma)];
+
+
+% q0 = c*cos(sigma);
+
+% if q0>=0
+%     q = [c*cos(sigma), -epsijk*s*cos(delta), -epsijk*s*sin(delta), -epsijk*c*sin(sigma)];
+% else
+%     q = [-c*cos(sigma), epsijk*s*cos(delta), epsijk*s*sin(delta), epsijk*c*sin(sigma)];
+% end
+
+
 if (abs(q(1))-0)<thr
     q(1)=0.0;
 elseif (abs(q(2))-0)<thr
@@ -38,3 +74,6 @@ elseif (abs(q(3))-0)<thr
 elseif (abs(q(4))-0)<thr
     q(4)=0.0;    
 end
+
+
+%}
