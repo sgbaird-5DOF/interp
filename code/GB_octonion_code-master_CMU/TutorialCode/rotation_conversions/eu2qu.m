ab6a19d243e2862a6639800cb8198393cb5674b2
sgbaird <45469701+sgbaird@users.noreply.github.com>
2020-08-16 00:36:44 -0600

    Update eu2qu.m

--- code/GB_octonion_code-master_CMU/TutorialCode/rotation_conversions/eu2qu.m
+++ code/GB_octonion_code-master_CMU/TutorialCode/rotation_conversions/eu2qu.m
@@ -1,48 +1,54 @@
 function q = eu2qu(eu)
 %--------------------------------------------------------------------------
-% Date:
+% Date: 2020-08-15
 %
 % Description:  from Euler angles to quaternions
 % 
 % Inputs:
-%  a - a
+%  eu - euler angles (XYZ convention I think?)
 %
 % Outputs:
-%  b - b
+%  q - quaternion (convention depends on epsijk value, see Notes)
 %
 % Usage:
-%  a = b(a);
+%  q = eu2qu(eu);
 %
 % Dependencies:
-%  *
+%  
 %
 % Notes:
-%  Vectorized by SGB 2020-08-15
+%  Vectorized by SGB 2020-08-15 epsijk can be set using the script
+%  setGlobal_epsijk.m, which controls whether the Morawiec convention is
+%  used or not.
 %--------------------------------------------------------------------------
+
+%epsijk setup/check
 global epsijk
-epsijk
 if isempty(epsijk)
 	epsijk = -1;
 	warning(['global variable epsijk not set. Using Bunge convention: epsijk == ' int2str(epsijk)])
 end
 
+%unpack angles
 aphi1 = eu(:,1);
 aPhi = eu(:,2);
 aphi2 = eu(:,3);
 
+%setup for quaternion calculation
 sigma = 0.5*(aphi1+aphi2);
 delta = 0.5*(aphi1-aphi2);
 c = cos(aPhi/2);
 s = sin(aPhi/2);
 
+%calculate quaternion
 q = [c.*cos(sigma), -epsijk*s.*cos(delta), -epsijk*s.*sin(delta), -epsijk*c.*sin(sigma)];
 
+%negate quaternions where q0 >= 0
 ids = q(:,1) >= 0;
 q(ids,:) = -q(ids,:);
 
 % set values very close to 0 as 0
 thr = 1e-10;
-
 q(abs(q)<thr) = 0;
 
 end
