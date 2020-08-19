43fa47a4f9a7e50d3887a6f8ab8b8e5c92261a10
sgbaird <45469701+sgbaird@users.noreply.github.com>
2020-08-19 01:39:17 -0600

    added epsijk functionality

--- code/GB_octonion_code-master_CMU/TutorialCode/octonion_functions/qmult.m
+++ code/GB_octonion_code-master_CMU/TutorialCode/octonion_functions/qmult.m
@@ -1,9 +1,14 @@
 function out = qmult(pp,qq)
+
+global epsijk
+if isempty(epsijk)
+	error('need to specify epsijk as global variable, e.g. via setGlobal_epsijk.m')
+end
 % multiply lists of quaternion pairs (input: rows of quaternions),
 % vectorized by SGB 2020-07-27
 p = pp(:,2:4); q = qq(:,2:4);
 qr = pp(:,1).*qq(:,1) - dot(p,q,2);
-qi = pp(:,1).*q + qq(:,1).*p + cross(p,q,2);
+qi = pp(:,1).*q + qq(:,1).*p + epsijk*cross(p,q,2);
 
 out = [qr qi];
 
