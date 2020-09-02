2c012b5775c542ae421cc2fb02c98e32b25edc36
sgbaird <45469701+sgbaird@users.noreply.github.com>
2020-09-01 22:54:23 -0600

    fixed bug
    was missing abs(), also changed if statement indices to be clearer, potentially fixed an issue.

--- code/GB_octonion_code-master_CMU/TutorialCode/rotation_conversions/qu2ax.m
+++ code/GB_octonion_code-master_CMU/TutorialCode/rotation_conversions/qu2ax.m
@@ -31,13 +31,14 @@ if any(ids2)
     q(ids2,:) = [qq(ids2,2), qq(ids2,3), qq(ids2,4), repelem(pi,nids2,1)];
 end
 
-if any(~ids2)
-    s = (qq(~ids2,1)./abs(qq(~ids2,1)))./rssq(qq(~ids2,2:4),2);
-    q(~ids2,:) = [qq(~ids2,2:4).*[s s s], omega(~ids2,:)];
+ids3 = ~ids & (qq(:,1) >= thr);
+if any(ids3)
+    s = (qq(ids3,1)./abs(qq(ids3,1)))./rssq(qq(ids3,2:4),2);
+    q(ids3,:) = [qq(ids3,2:4).*[s s s], omega(ids3,:)];
 end
 
 % set values very close to 0 as 0
-q(q < thr) = 0;
+q(abs(q) < thr) = 0; %was missing abs() 2020-09-01 SGB
 
 end
 
