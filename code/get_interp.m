bf6feda7fa2964b2742fa5c382e9335a16a8c777
sgbaird <45469701+sgbaird@users.noreply.github.com>
2020-09-03 14:51:01 -0600

    updated preamble

--- code/get_interp.m
+++ code/get_interp.m
@@ -12,24 +12,63 @@ end
 %
 % Date: 2020-07-27
 %
-% Description: get barycentric interpolation values
+% Description: Interpolate query point property values based on their
+% spherical or planar barycentric coordinates relative to a mesh.
 %
 % Inputs:
-%	a - a
+%	mesh - struct containing octonions (pts), down-projected octonions
+%	(ppts), property values (props), a triangulation (sphK), and a filename
+%	(fname) for the "predictors" and "predictor responses"
+%
+%	data - struct containing octonions (pts), down-projected octonions
+%	(ppts), property values which can be all NaNs (props), and a (fname)
+%	for the query points and interpolated values
+%
+%   intfacetIDs - intersecting facet IDs of data.ppts relative to mesh.ppts
+%   and mesh.sphK
+%
+%   barytype - type of barycentric interpolation, 'spherical' or 'planar'
+%
+%   barytol - tolerance for barycentric coordinates, defaults to 0.2 for
+%   spherical and 1e-6 for planar. *
 %
 % Outputs:
-%	b - b
+%	datainterp - interpolated property values at the query points
+%
+%   databary - barycentric coordinates of query points relative to mesh
+%
+%   savename - catenatation of mesh.fname and data.fname where the
+%   workspace is saved
+%
+%   varargout{1} - nndistList, NN euclidean distances
+%
+%   varargout{2} - nonintDists, NN euclidean distances for non-intersecting
+%   points
 %
 % Usage:
-%		a = b(a);
+%   [datainterp,databary,savename,varargout] =
+%   get_interp(mesh,data,intfacetIDs,barytype,barytol);
 %
 % Dependencies:
-%		*
+%   get_omega.m
+%
+%   projray2hypersphere.m
+%
+%   sphbary.m
+%    -projfacet2hyperplane.m
+%     --projray2hyperplane.m
+%
+%   sqrt2norm.m
 %
 % Notes:
-%		consider adding option for 'bary' vs. 'knn' interpolation
+%	consider adding option for 'bary' vs. 'knn' interpolation
+%
+%   *0.2 was chosen as the spherical barytol default because it decreased
+%   the interpolation error relative to planar, whereas lower tolerances
+%   produced more non-intersections.
 %--------------------------------------------------------------------------
 if isempty(barytol)
+    %assign default barytol
     switch barytype
         case 'spherical'
             barytol = 0.2;
@@ -38,9 +77,11 @@ if isempty(barytol)
     end
 end
 
+%unpack
 meshpts = mesh.ppts;
 datapts = data.ppts;
 
+%check dimensions
 if size(mesh.ppts,2) ~= size(data.ppts,2)
 	errmsg = ['mesh.pts and data.pts dims should be equal, but mesh dim == ' ...
 		int2str(size(mesh.ppts,2)) ' and data dim == ' int2str(size(data.ppts,2))];
@@ -58,7 +99,6 @@ nndistList = get_omega(sqrt2norm(mesh.pts(nnList,:)),sqrt2norm(data.pts));
 databary = NaN(size(datapts));
 facetprops = databary;
 datainterp = NaN(size(datapts,1),1);
-% nndistList = datainterp;
 nonintDists = datainterp;
 nnID = [];
 ilist = [];
@@ -69,7 +109,7 @@ for i = 1:ndatapts
 	datapt = datapts(i,:); %use down-projected data (and mesh)
 	baryOK = false; %initialize
 	if ~isempty(intfacetIDs{i})
-		%setup
+		%% setup
 		intfacetID = intfacetIDs{i}(1); %take only the first intersecting facet? Average values? Use oSLERP instead?
 		vtxIDs = mesh.sphK(intfacetID,:);
 		facet = meshpts(vtxIDs,:); %vertices of facet
@@ -96,7 +136,7 @@ for i = 1:ndatapts
 		end
 		
 		if baryOK
-			%% interpolate using bary coords
+			% interpolate using bary coords
 			datainterp(i) = dot(databary(i,:),facetprops(i,:));
 		else
 			disp([num2str(databary(i,:),2) ' ... sum == ' num2str(sum(databary(i,:)),10)]);
@@ -135,7 +175,7 @@ disp(' ')
 disp(['RMSE (J/m^2): interp == ' num2str(interpRMSE,'%3.4f') ', NN == ' num2str(nnRMSE,'%3.4f')])
 disp(' ')
 disp(['total RMSE: ' num2str(totRMSE,'%3.4f') ', all NN RMSE comparison: ' num2str(allnnRMSE,'%3.4f')])
-varargout = {nndistList,nonintDists,intfacetIDs};
+varargout = {nndistList,nonintDists};
 
 end
 
@@ -158,5 +198,14 @@ disp('get intersecting facets')
 intfacetIDs = intersect_facet(meshpts,mesh.K,datapts,inttol,maxnormQ);
 
 % 		prop = data.props(i,:);
+
+% % initialize
+% databary = NaN(size(datapts));
+% facetprops = databary;
+% datainterp = NaN(size(datapts,1),1);
+% nndistList = datainterp;
+% nonintDists = datainterp;
+% nnID = [];
+% ilist = [];
 
 %}
