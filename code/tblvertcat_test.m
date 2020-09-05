dba7023548eff645ebc02d1ad18bf1af8d76de5f
sgbaird <45469701+sgbaird@users.noreply.github.com>
2020-09-05 00:50:35 -0600

    Update tblvertcat_test.m

--- code/tblvertcat_test.m
+++ code/tblvertcat_test.m
@@ -5,7 +5,7 @@ nrows = 3;
 %define table columns
 doubles = rand(nrows,1);
 chars = repelem('a',nrows,1);
-cells = repelem(cell(1,1),nrows,1);
+cells = repelem({rand(10)},nrows,1);
 
 %make two tables
 tbl1 = table(doubles,chars);
