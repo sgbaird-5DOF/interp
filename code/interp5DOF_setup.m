5a7f9c9f9bbac713f414b38e42e91f228975207e
sgbaird <45469701+sgbaird@users.noreply.github.com>
2020-09-12 00:35:13 -0600

    update MATslurm

---
+++ code/interp5DOF_setup.m
@@ -0,0 +1,92 @@
+function [propOut,interpfn,mdl,mdlpars] = interp5DOF_setup(ndatapts,npredpts,method,uuid,inputtype)
+arguments
+   ndatapts
+   npredpts
+   method = 'gpr'
+   uuid = get_uuid()
+   inputtype char {mustBeMember(inputtype,{'5dof','octonion'})} = '5dof'
+end
+%INTERP5DOF_SETUP  setup for interpolating five-degree-of-freedom property
+%data using random octochorically sampled octonions
+
+%% setup
+addpathdir({'cu2qu.m','q2rod.m','qmult.m','get_ocubo.m'})
+
+%random octonions
+ocubotype = 'random';
+ocuboseed1 = 8;
+ocuboseed2 = 15;
+o = get_ocubo(ndatapts,ocubotype,[],ocuboseed1);
+o2 = get_ocubo(npredpts,ocubotype,[],ocuboseed2);
+
+% o = get_ocubo(ndatapts,'random');
+% o2 = get_ocubo(npredpts,'random');
+genseed = 10;
+rng(genseed);
+
+%convert to 5DOF
+oct2fivemethod = 'simple'; %'reverse', 'simple'
+five = GBoct2five(o,true,oct2fivemethod);
+five2 = GBoct2five(o2,true,oct2fivemethod);
+
+%get BRK function values
+propList = GB5DOF_setup(five);
+
+switch inputtype
+    case '5dof'
+        %unpack
+        qm = vertcat(five.q);
+        nA = vertcat(five.nA);
+        qm2 = vertcat(five2.q);
+        nA2 = vertcat(five2.nA);
+    case 'octonion'
+        [qm,nA,qm2,nA2]=deal([]);
+end
+
+%% interpolation
+[propOut,interpfn,mdl,mdlpars] = interp5DOF(qm,nA,propList,qm2,nA2,method,'uuid',uuid,'o',o,'o2',o2);
+
+%% error values
+proptrue = mdl.data.props;
+
+e = proptrue-propOut; %error
+ae = abs(e); %absolute error
+mae = mean(ae); %mean absolute error
+se = e.^2; %square error
+mse = mean(se); %mean square error
+rmse = sqrt(mse); %root mean square error
+disp(['RMSE = ' num2str(rmse) ' J/m^2'])
+disp(['MAE = ' num2str(mae) ' J/m^2'])
+errmetrics = var_names(e,ae,mae,se,mse,rmse);
+
+%% repackage model and parameters
+mdlparsextra = var_names(ocubotype,ocuboseed1,ocuboseed2,genseed,errmetrics,rmse,mae,oct2fivemethod);
+mdlextra = var_names(ocubotype,ocuboseed1,ocuboseed2,genseed,errmetrics,rmse,mae,oct2fivemethod);
+
+%function to concatenate structures with all different fields
+structcat = @(S1,S2) table2struct([struct2table(S1,'AsArray',true),struct2table(S2,'AsArray',true)]);
+
+%concatenation
+mdlpars = structcat(mdlpars,mdlparsextra);
+mdl = structcat(mdl,mdlextra);
+end
+
+
+%---------------------------------CODE GRAVEYARD---------------------------
+%{
+switch method
+    case 'gpr'
+        [propOut,mdl,ysd,yint] = interp5DOF(qm,nA,propList,qm2,nA2,method);
+        mdl.ysd = ysd;
+        mdl.yint = yint;
+    case {'sphbary','pbary'}
+        [propOut,mdl.databary,mdl.facetprops,mdl.fname] = interp5DOF(qm,nA,propList,qm2,nA2,method);
+    case 'nn'
+        [propOut,mdl] = interp5DOF(qm,nA,propList,qm2,nA2,method);
+end
+
+% mse = immse(propList2,propOut);
+
+
+% ocuboOpts = var_names(ocubotype,ocuboseed1,ocuboseed2,genseed);
+%}
---
+++ code/randOctParityData.m
@@ -0,0 +1,207 @@
+clear; close all
+
+addpathdir('var_names.m')
+runtype = 'test'; %'test','full'
+switch runtype
+    case 'test'
+        ndatapts = 388;
+        npredpts = 100;
+        method = {'sphbary','gpr'};
+        inputtype = {'5dof','octonion'};
+    case 'full'
+        ndatapts = [10000 20000 50000];
+        npredpts = 1000;
+        method = {'gpr'}; %'sphbary','pbary','gpr','nn'
+end
+
+%% functions to generate save filepaths
+%diary
+diaryfolder = fullfile('data','randOctParity','diary');
+diarynamefn = @(method,ndatapts,gitcommit,puuid) [method int2str(ndatapts) '_gitID-' gitcommit(1:7) '_puuID-' puuid '.txt'];
+diarypathfn = @(method,ndatapts,gitcommit,puuid) fullfile(diaryfolder,diarynamefn(method,ndatapts,gitcommit,puuid));
+%data
+savefolder = fullfile('data','randOctParity','pcombs');
+savenamefn = @(method,ndatapts,gitcommit,puuid) [method int2str(ndatapts) '_gitID-' gitcommit(1:7) '_puuID-' puuid '.mat'];
+
+savepathgen = fullfile(savefolder,'*gitID-*puuID*.mat'); %for use with dir
+savenamematch = [... %for use with regexp
+    ['(' strjoin(method,'|') ')'] ... match (exactly) any of the method options
+    ['(' strjoin(num2cell(num2str(ndatapts),2),'|') ')'] ... match (exactly) any of the ndatapts options
+    '(_gitID-[a-z0-9]*)' ... match any combination of 0 or more lowercase alphabetic or numeric characters (for git hash)
+    '(_puuID-[a-z0-9]+)' ... match any combination of 1 or more lowercase alphabetic or numeric characters (for param combo uuid)
+    '[.mat]' ... %match (exactly) the file-extension
+    ]; %e.g. '(sphbary|gpr)(50)(_gitID-)[a-z0-9]*)(_puuID-[a-z0-9]+).mat'
+
+savepathfn = @(method,ndatapts,gitcommit,puuid) fullfile(savefolder,savenamefn(method,ndatapts,gitcommit,puuid));
+
+%% parameter file setup
+%parameters
+pars = var_names(ndatapts,npredpts,method,inputtype); %add all parameters here (see runtype switch statement)
+%function to execute and output arguments from function
+execfn = @(ndatapts,npredpts,method,inputtype) ...
+    interp5DOF_setup(ndatapts,npredpts,method,get_uuid(),inputtype); %names need to match pars fields
+argoutnames = {'propOut','interpfn','mdl','mdlpars'};
+%i.e. [propOut,interpfn,mdl,mdlpars] = interp5DOF_setup(ndatapts,npredpts,method);
+
+walltimefn = @() 600; %can set to constant or to depend on parameters
+
+%% parameter file
+[parpath, parcombsets, Ntrim, jobwalltimes] = ...
+    writeparfile(pars,execfn,argoutnames,walltimefn,'diarypathfn',diarypathfn,'savepathfn',savepathfn);
+
+%% job submission
+env = 'local'; %'slurm', 'local'
+switch env
+    case 'slurm'
+        %setup
+        cores = 12;
+        mem = 1024*4*cores; %total memory of job, MB
+        qosopt = 'standby'; %'', 'test', 'standby'
+        %submission
+        submit_sbatch(parpath,cores,mem,qosopt,'submit.sh'); %submit.sh --> exec_combs.m --> execfn (defined above)
+        
+    case 'local'
+        %nested loop through jobs and tasks to generate results
+        for jid = 1:length(Ntrim)
+            N = Ntrim(jid);
+            for tid = 1:N
+                outtmp = exec_combs(parpath, jid, tid);
+            end
+        end
+        %get names and folders from files that match general savepath
+        files = dir(savepathgen);
+        namestmp = {files.name};
+        folderstmp = {files.folder};
+        
+        %get names and folders of filenames that match savenamematch
+        startIDs = regexp(namestmp,savenamematch);
+        matchIDs = ~cellfun(@isempty,startIDs);
+        names = namestmp(matchIDs);
+        folders = folderstmp(matchIDs);
+        nfiles = length(names);
+        assert(nfiles ~= 0,'no files matched')
+        %initialize
+        init1 = cell(nfiles,1);
+        [outlist,propOutlist,mdllist,mdlparslist,interpfnlist] = deal(init1);
+        
+        %extract results from files
+        for i = 1:nfiles
+            folder = folders{i};
+            name = names{i};
+            fpath = fullfile(folder,name);
+            clear S out
+            S = load(fpath,'out');
+            out = S.out;
+            [propOutlist{i},mdllist{i},mdlparslist{i},interpfnlist{i}] = ...
+                deal(out.propOut, out.mdl, out.mdlpars, out.interpfn);
+            outlist{i} = out;
+        end
+        
+        %concatenate models and parameters
+        mdlcat = structvertcat(mdllist{:});
+        mdlparscat = structvertcat(mdlparslist{:});
+        mdlparstbl = struct2table(mdlparscat,'AsArray',true);
+        
+        gitcommit = get_gitcommit();
+        %save models and parameters
+        fpath = fullfile(savefolder,['gitID-' get_gitcommit '_uuID-' get_uuid() '.mat']);
+        save(fpath,'propOutlist','interpfnlist','mdllist',...
+            'mdlparslist','mdlcat','mdlparscat','mdlparstbl','outlist',...
+            '-v7.3','-nocompression')
+        writetable(mdlparstbl,[fpath(1:end-4) '.xlsx'])
+        
+        %         %nested loop through jobs and tasks to load results
+        %         S = load(parpath);
+        %         for jid = 1:length(Ntrim)
+        %             for tid = 1:jid
+        %                 S.
+        %             end
+        %         end
+end
+
+disp('end randOctParityData.m')
+disp(' ')
+%-----------------------------CODE GRAVEYARD-------------------------------
+%{
+
+
+for i = 1:length(ndataptsList)
+    %unpack
+    ndatapts = ndataptsList(i);
+    for j = 1:length(methods)
+        method = methods{j};
+        fname = ['randOctParityData_' method int2str(ndatapts) '.mat'];
+        S = load(
+    end
+end
+
+% execfn = @interp5DOF_setup;
+
+%initialize
+init1 = cell(length(ndatapts),length(method));
+propOutlist = init1;
+mdllist = init1;
+mdlparslist = init1;
+interpfnlist = init1;
+
+
+% %nested loop through ndataptsList and methodlist
+% for i = 1:length(ndatapts)
+%     %unpack
+%     ndatapts = ndatapts(i);
+%     for j = 1:length(method)
+%         %% setup
+%         %unpack
+%         method = method{j};
+%         disp(method)
+%
+%         %get a unique ID
+%         uuid = get_uuid();
+%
+%         %% interpolation
+%         [propOutlist{i,j},interpfnlist{i,j},mdllist{i,j},mdlparslist{i,j}]=interp5DOF_setup(ndatapts,npredpts,method,uuid);
+%
+%         %% output
+%         %package
+%         propOut = propOutlist{i,j};
+%         interpfn = interpfnlist{i,j};
+%         mdl = mdllist{i,j};
+%         mdlpars = mdlparslist{i,j};
+%         %save
+%         save(savepathfn(method,ndatapts,mdl.gitcommit,uuid),...
+%             'propOut','interpfn','mdl','mdlpars','ndatapts','method','uuID',...
+%             '-v7.3','-nocompression')
+%         disp(' ')
+%     end
+% end
+
+%git commit
+[status,cmdout] = system('git log -1 --oneline');
+if status == 0
+    gitcommit = cmdout;
+else
+    gitcommit = '';
+end
+
+%job universal unique ID
+uuid = {get_uuid()};
+
+pars = var_names(ndatapts,npredpts,method.uuid);
+
+%         S(nfiles) = struct();
+%         S(1).out = struct();
+
+
+memfn = @(method,ndatapts) ...
+    (strcmp(method,'gpr') && ndatapts > 10000) *1024*8 + ...
+    (~strcmp(method,'gpr') && ndatapts <= 10000) * 1024*4;
+
+%         k = 0; %initialize counter
+        %nested loop through jobs and tasks to generate results
+        for jid = 1:length(Ntrim)
+            for tid = 1:jid
+%                 k = k+1;
+                outtmp = exec_combs(parpath, jid, tid);
+            end
+        end
+%}
