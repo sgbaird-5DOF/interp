0a10dcdf0c8cf979fbe1b509fbbc73c77376721c
sgbaird <45469701+sgbaird@users.noreply.github.com>
2020-09-15 19:16:12 -0600

    delete var_names.m

--- code/var_names.m
+++
@@ -1,8 +0,0 @@
-function out = var_names(varargin)
-%--------------------------------------------------------------------------
-% https://www.mathworks.com/matlabcentral/answers/79281#answer_89015 
-%--------------------------------------------------------------------------
-for n = 1:nargin
-	out.(inputname(n)) = varargin{n};
-end
-end
