36d48fe137be90d0d2a3672c0fdf36472ae80fb7
sgbaird <45469701+sgbaird@users.noreply.github.com>
2020-09-22 19:19:09 -0600

    first refactor
    argument validation
    use normr.m
    make nA_Lab rows of normals instead of columns (and update cross(), etc.) to be consistent with interp5DOF

--- code/GBlab2oct.m
+++ code/GBlab2oct.m
@@ -1,13 +1,15 @@
-%-------------------------------------------------------------------------%
-% Filename: GBlab2oct.m
-% Author: Oliver Johnson
-% Date: 9/22/2020
-%
-% GBLAB2OCT will convert the crystal orientations (expressed as 
+function oAB = GBlab2oct(qA_Lab,qB_Lab,nA_Lab,convention)
+arguments
+   qA_Lab(:,4) double {mustBeReal,mustBeFinite,mustBeRowNormalized}
+   qB_Lab(:,4) double {mustBeReal,mustBeFinite,mustBeRowNormalized}
+   nA_Lab(:,3) double {mustBeReal,mustBeFinite}
+   convention char {mustBeMember(convention,'francis')} = 'francis'
+end
+% GBLAB2OCT  Convert the crystal orientations (expressed as 
 % quaternions) of grains meeting at a grain boundary (GB) together with the
 % corresponding GB normals to the GB octonion defined by Francis [1] and 
 % required by the GBdist function [2].
-%
+%-------------------------------------------------------------------------%
 % NOTE: The convention used by [1] and required for proper use of the 
 % GBdist function [2] is that the GB plane reference frame is defined such 
 % that the GB plane lies in the zR = 0 plane, with grain A occupying zR > 0
@@ -48,24 +50,25 @@
 %         each octonion is sqrt(2). This is the expected format of
 %         octonions used as input to GBdist.
 %
+% Usage:
+%   oAB = GBlab2oct(qA_Lab,qB_Lab,nA_Lab,convention)
+%
 % [1] T. Francis, I. Chesser, S. Singh, E.A. Holm, M. De Graef, A geodesic 
 %     octonion metric for grain boundaries, Acta Mater. 166 (2019) 135–147.
 %     doi:10.1016/j.actamat.2018.12.034.
 % [2] https://github.com/ichesser/GB_octonion_code
+%
+% Filename: GBlab2oct.m
+% Author: Oliver Johnson, Refactored by Sterling Baird
+% Date: 9/22/2020
 %-------------------------------------------------------------------------%
 
-function oAB = GBlab2oct(qA_Lab,qB_Lab,nA_Lab,convention)
-
-%% Ensure proper formatting of inputs
-
-assert(size(qA_Lab,2) == 4 && size(qB_Lab,2) == 4,'qA_Lab and qB_Lab must be n-by-4 arrays of quaternions.')
-assert(size(nA_Lab,1) == 3,'nA_Lab must be a 3-by-n array of vectors.')
-
+%% Setup
 % number of GBs
 Ngb = size(qA_Lab,1);
 
 % ensure GB normals are normalized
-nA_Lab = nA_Lab./sqrt(sum(nA_Lab.^2,1));
+nA_Lab = normr(nA_Lab);
 
 % normals pointing away from grain B in the Lab frame
 nB_Lab = -nA_Lab;
@@ -76,8 +79,8 @@ switch convention
     case 'francis' % francis convention has z-axis aligned to nB_Lab
         zR = nB_Lab;
         
-        xR = cross(zR,rand(3,Ngb)); % cross with random vector to get an arbitrary vector in the GB plane to use as the x-axis
-        xR = xR./sqrt(sum(xR.^2,1));
+        xR = cross(zR,rand(Ngb,3),2); % cross with random vector to get an arbitrary vector in the GB plane to use as the x-axis
+        xR = normr(xR);
 %     case 'olmsted' % olmsted convention has z-axis aligned to -nB_Lab
 %         zR = -nB_Lab;
 %         
@@ -90,8 +93,8 @@ switch convention
 %         zR = zR./sqrt(sum(zR.^2,1));
 end
 
-yR = cross(zR,xR); % cross z with x to define y-axis
-yR = yR./sqrt(sum(yR.^2,1));
+yR = cross(zR,xR,2); % cross z with x to define y-axis
+yR = normr(yR);
 
 % make rotation matrices
 gR = [permute(xR,[1,3,2]),permute(yR,[1,3,2]),permute(zR,[1,3,2])];
@@ -109,3 +112,32 @@ qB_R = qmultiply(qB_Lab,qinv(qR));
 oAB = [qA_R, qB_R];
 
 end
+
+%% Argument Validation Function(s)
+function mustBeRowNormalized(X,tol)
+e = X-normr(X); %error
+ae = abs(e); %absolute error
+tolcheck = ae < tol; %tolerance check
+badIDs = find(tolcheck); %number of "bad" rows
+nbad = length(badIDs);
+if ~isempty(nbad)
+    avgnm = mean(norm(X,2));
+    error([int2str(nbad) ' rows where norm ~= 1 starting at element ' ...
+        int2str(badIDs(1)) ', avgnm == ' int2str(avgnm)])
+end
+end
+
+%% CODE GRAVEYARD
+%{
+assert(size(qA_Lab,2) == 4 && size(qB_Lab,2) == 4,'qA_Lab and qB_Lab must be n-by-4 arrays of quaternions.')
+assert(size(nA_Lab,1) == 3,'nA_Lab must be a 3-by-n array of vectors.') 
+
+%% Ensure proper formatting of inputs
+
+
+% nA_Lab = nA_Lab./sqrt(sum(nA_Lab.^2,1));
+
+%         xR = xR./sqrt(sum(xR.^2,1));
+
+yR = yR./sqrt(sum(yR.^2,1));
+%}
