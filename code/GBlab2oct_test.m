5030efb20fe802f0e96a55edb3339defedb28f1b
sgbaird <45469701+sgbaird@users.noreply.github.com>
2020-09-22 21:42:35 -0600

    use addpathdir instead of addpath@genpath

--- code/GBlab2oct_test.m
+++ code/GBlab2oct_test.m
@@ -1,8 +1,8 @@
 %% Test from GB Octonion Tutorial script
-% note: this requires functions from the GB_octonion_code-master
+% note: this requires functions from the GB_octonion_code
 % repository, specifically the ax2qu.m function
 
-addpath(genpath('GB_octonion_code-master'))
+addpathdir('ax2qu.m')
 
 aaAbp = [0 1 0 atan(1/5)]; % grain orientations in GB plane reference frame
 aaBbp = [0 1 0 -atan(1/5)];
@@ -19,24 +19,39 @@ o2 = 1/sqrt(2)*[qCbp qDbp];
 Omega = rad2deg(2*acos(dot(o1,o2)))
 
 %% Test using our functions with GBdist
+qA_Lab = rot2q(atan(1/5),pi/2,pi/2);
+qB_Lab = rot2q(-atan(1/5),pi/2,pi/2);
+nA_Lab = [0 0 1];
+
+oAB = GBlab2oct(qA_Lab,qB_Lab,nA_Lab,'francis');
+
+qC_Lab = rot2q(atan(1/2),pi/2,pi/2);
+qD_Lab = rot2q(-atan(1/2),pi/2,pi/2);
+nC_Lab = [0 0 1];
+
+oCD = GBlab2oct(qC_Lab,qD_Lab,nC_Lab,'francis');
+
+OmegaTest = rad2deg(GBdist([oAB,oCD],32,false))
+
+OmegaTest2 = rad2deg(GBdist4(oAB,oCD,32,'omega'))
+
+
+%----------------------------CODE GRAVEYARD--------------------------------
+%{
+
 % note: this requires functions from Dr. Johnson's quaternion library, AND 
 % you should remove the GB_octonion_code-master from the search path since 
 % it has some files with the same name as those required here)
 
-rmpath(genpath('GB_octonion_code-master')) % remove from path so that GBlab2oct uses Dr. Johnson's qinv function
+% addpath(genpath('GB_octonion_code-master_CMU'))
+% rmpath(genpath('GB_octonion_code-master')) % remove from path so that GBlab2oct uses Dr. Johnson's qinv function
+% addpath(genpath('GB_octonion_code-master')) % add back to path to use GBdist
 
-qA_Lab = rot2q(atan(1/5),pi/2,pi/2);
-qB_Lab = rot2q(-atan(1/5),pi/2,pi/2);
 nA_Lab = [0 0 1].';
 
 oAB = GBlab2oct(qA_Lab,qB_Lab,nA_Lab(:),'francis');
 
-qC_Lab = rot2q(atan(1/2),pi/2,pi/2);
-qD_Lab = rot2q(-atan(1/2),pi/2,pi/2);
 nC_Lab = [0 0 1].';
 
 oCD = GBlab2oct(qC_Lab,qD_Lab,nC_Lab(:),'francis');
-
-addpath(genpath('GB_octonion_code-master')) % add back to path to use GBdist
-
-OmegaTest = rad2deg(GBdist([oAB,oCD],30,false))
+%}
