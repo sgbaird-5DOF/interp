806dda5557dc2a256725d9c4fb4aec447f3d0dd9
sgbaird <45469701+sgbaird@users.noreply.github.com>
2020-10-10 03:09:09 -0600

    remove GBoct2five dependencies

--- code/interp5DOF_test.m
+++ code/interp5DOF_test.m
@@ -3,27 +3,27 @@ clear; close all
 
 testnum = 1;
 
-addpathdir({'cu2qu.m','q2rod.m','qmult.m','get_uuid.m'})
+addpathdir({'cu2qu.m','q2rod.m','qmult.m','get_uuid.m','qmA2nA.m'})
 
 switch testnum
     case 1
         %random octonions
         ndatapts = 388;
-        npredpts = 388;
-        o = get_ocubo(ndatapts,'random',[],8);
-        o2 = get_ocubo(npredpts,'random',[],15);
-%         o = get_ocubo(ndatapts);
-%         o2 = get_ocubo(npredpts);
-         
-        %convert to 5DOF
-        five = GBoct2five(o);
-        five2 = GBoct2five(o2);
+        npredpts = 10000;
+%         o = get_ocubo(ndatapts,'random',[],8);
+%         o2 = get_ocubo(npredpts,'random',[],15);
+        five = get_five(ndatapts);
+        five2 = get_five(npredpts);
         
         %unpack
         qm = vertcat(five.q);
         nA = vertcat(five.nA);
         qm2 = vertcat(five2.q);
         nA2 = vertcat(five2.nA);
+        
+        %convert to octonions
+        o = GBfive2oct(qm,nA);
+        o2 = GBfive2oct(qm2,nA2);
         
         %get BRK function values
         propList = GB5DOF_setup(five);
@@ -47,4 +47,12 @@ parityplot(propOut,propList2)
 %{
 %         rmse = sqrt(immse(propList2,propOut));
 %         mae = mean(abs(propList2-propOut));
+
+
+%         o = get_ocubo(ndatapts);
+%         o2 = get_ocubo(npredpts);
+         
+        %convert to 5DOF
+%         five = GBoct2five(o);
+%         five2 = GBoct2five(o2);
 %}
