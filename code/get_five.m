6b7f7ebac059ec290dde143110fe8b258629bd7f
sgbaird <45469701+sgbaird@users.noreply.github.com>
2020-10-10 01:08:04 -0600

    Abandon to 5dof (#8)
    * test change for GitHub Desktop branches
    
    * option to use johnson or francis
    
    * Update randOctParityData.m
    
    * Update GBfive2oct.m
    
    * Create gpr388_gitID-d83e690_puuID-b5e4795a.txt
    
    * Create get_five.m
    
    * Update interp5DOF.m
    
    * Update interp5DOF_setup.m
    
    * Create uuID-ad4a1bf8.mat
    
    * Create uuID-d5b839a8.mat
    
    * Update MATslurm

---
+++ code/get_five.m
@@ -0,0 +1,30 @@
+function five = get_five(npts)
+arguments
+    npts(1,1) double = 1
+end
+% GET_FIVE  generate random 5DOF misorientation/boundary plane normal pairs,
+% where the misorientation is cubochorically randomly sampled.
+%--------------------------------------------------------------------------
+% Inputs:
+%  npts - number of 5DOF points to generate
+%
+% Outputs:
+%  five - struct containing at minimum misorientations and boundary plane
+%  normals.
+%
+% Usage:
+%  five = get_five(npts);
+%
+% Dependencies:
+%  get_cubo.m
+%
+% Notes:
+%  *
+%
+% Author(s): Sterling Baird
+%
+% Date: 2020-10-06
+%--------------------------------------------------------------------------
+five.q = get_cubo(npts);
+five.nA = normr(rand(npts,3)-0.5);
+end
