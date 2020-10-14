1483b5ab8e9272fe13c18e2d53e80f4475bf5266
sgbaird <45469701+sgbaird@users.noreply.github.com>
2020-10-14 13:05:45 -0600

    interp functions

--- code/interpfns/get_ppts.m
+++ code/interpfns/get_ppts.m
@@ -1,3 +1,3 @@
-function ppts = get_ppts(qm,nA,projtol,usv)
-ppts = proj_down(get_octpairs(GBfive2oct(qm,nA)),projtol,usv);
+function ppts = get_ppts(pts,projtol,usv,zeroQ)
+ppts = proj_down(pts,projtol,usv,'zeroQ',zeroQ);
 end
--- code/interpfns/interp_nn.m
+++ code/interpfns/interp_nn.m
@@ -1,9 +1,10 @@
-function props = interp_nn(ppts,qm2,nA2,projtol,usv,propList)
+function props = interp_nn(ppts,qm2,nA2,projtol,usv,zeroQ,propList)
 
 if isempty(usv)
     error('usv should not be empty')
 end
-props = propList(dsearchn(ppts,get_ppts(qm2,nA2,projtol,usv)));
+pts = get_pts(qm2,nA2);
+props = propList(dsearchn(ppts,get_ppts(pts,projtol,usv,zeroQ)));
 
 end
 
