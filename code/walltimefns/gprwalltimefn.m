6862a47817bfb9ba83ca52138f901de66f8b34e0
sgbaird <45469701+sgbaird@users.noreply.github.com>
2020-10-15 02:33:52 -0600

    lengthen gprwalltime

--- code/walltimefns/gprwalltimefn.m
+++ code/walltimefns/gprwalltimefn.m
@@ -1,13 +1,13 @@
 function walltime = gprwalltimefn(ndatapts,cores)
 switch ndatapts
     case num2cell(1:1000)
-        walltime = 5;
+        walltime = 5*cores;
     case num2cell(1001:5000)
-        walltime = 8;
+        walltime = 8*cores;
     case num2cell(5001:10000)
-        walltime = 12;
+        walltime = 12*cores;
     case num2cell(10001:50000)
-        walltime = 20;
+        walltime = 20*cores;
     otherwise
         error('too many datapts, update gprwalltimefn')
 end
