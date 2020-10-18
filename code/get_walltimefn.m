120237f0f08d185f950478968aa622e9f145ff6e
sgbaird <45469701+sgbaird@users.noreply.github.com>
2020-10-17 23:19:21 -0600

    added 'idw' to walltimefn

--- code/get_walltimefn.m
+++ code/get_walltimefn.m
@@ -2,7 +2,7 @@ function walltimefn = get_walltimefn(ndatapts,npredpts,method,cores) %#ok<*INUSD
 arguments
     ndatapts(1,1) double
     npredpts(1,1) double
-    method char {mustBeMember(method,{'sphgpr','gpr','pbary','sphbary','nn','avg'})}
+    method char {mustBeMember(method,{'sphgpr','gpr','pbary','sphbary','idw','nn','avg'})}
     cores(1,1) double
 end
 walltimeBuffer = 5;
@@ -12,6 +12,8 @@ switch method
         walltimefn = gprwalltimefn(ndatapts,cores) + walltimeBuffer;
     case {'pbary','sphbary'}
         walltimefn = barywalltimefn(ndatapts,npredpts,cores) + walltimeBuffer;
+    case {'idw'}
+        walltimefn = walltimeBuffer*2;
     case {'nn'}
         walltimefn = walltimeBuffer;
     case {'avg'}
