75bc2326579935e7080386df7432b1875f6cb985
sgbaird <45469701+sgbaird@users.noreply.github.com>
2020-10-19 18:15:05 -0600

    Update plotting.m

--- code/plotting.m
+++ code/plotting.m
@@ -9,6 +9,48 @@ yactual = padcat(tbltmp.data.props(barypars.ids),tbltmp.data.props(barypars.ilis
 ypred = padcat(tbltmp.propOut{1}(barypars.ids),tbltmp.propOut{1}(barypars.ilist));
 parityplot(yactual,ypred,'title','sphbary','legend',{'interp','nn'})
 
+%split apply & find groups
+fname = 'gitID-76fca8c_uuID-f51500cd_paper-data.mat';
+files = dir(fullfile('**',fname));
+fpath = fullfile(files(1).folder,files(1).name);
+load(fpath);
+tbl1 = mdlparstbl(:,{'method','nmeshpts','ndatapts'});
+[G1,ID1] = findgroups(tbl1);
+parity = splitapply(@(x){x(1)},mdlparstbl.parity,G1);
+nmeshpts = splitapply(@(x)x(1),mdlparstbl.nmeshpts,G1);
+medRMSE = splitapply(@(x)median(x),mdlparstbl.rmse,G1);
+medMAE = splitapply(@(x)median(x),mdlparstbl.mae,G1);
+medruntime = splitapply(@(x)median(x),mdlparstbl.runtime,G1);
+
+tbl2 = mdlparstbl(:,{'method','ndatapts'});
+[G2,ID2] = findgroups(ID1.method);
+nmeshpts1 = splitapply(@(x){x},mdlparstbl.nmeshpts,G2);
+
+
+%% parity plot
+methodlist = {'pbary','gpr','idw','nn'};
+for nmeshpts = [388 10000 50000]
+    %extract parity and IDs
+    tbl3 = mdlparstbl(ismember(mdlparstbl.method,methodlist) & mdlparstbl.nmeshpts==nmeshpts,:);
+    [G3,ID3] = findgroups(tbl3.method);
+    parity3 = splitapply(@(x){x(1)},tbl3.parity,G3);
+    
+    %plotting
+    multiparity(parity3,ID3)
+    
+    %extra
+    sgtitle(['nmeshpts = ' int2str(nmeshpts)])
+    
+    %saving
+    files = dir(fullfile('**','interp5DOF-paper','figures'));
+    folder = files(1).folder;
+    fname = ['brkparity',int2str(nmeshpts)];
+    fpath = fullfile(folder,fname);
+    savefig(fpath)
+    print(fpath,'-dpng')
+end
+    
+
 %% distance parity
 pd1 = pdist(mdltbl(strcmp(mdltbl.uuid,uuid),:).mesh{1}.pts).';
 pd2 = pdist(mdltbl(strcmp(mdltbl.uuid,uuid),:).mesh{1}.pts,@get_omega).';
@@ -25,3 +67,60 @@ figure
 t=n2c(pts);
 scatter3(t{:})
 axis equal
+
+
+%% CODE GRAVEYARD
+%{
+%split apply & find groups
+fname = 'gitID-6a89f2b_uuID-585a81b1_paper-data.mat';
+files = dir(fullfile('**',fname));
+fpath = fullfile(files(1).folder,files(1).name);
+load(fpath);
+[G1,ID1] = findgroups(mdlparstbl.(:,{'method','ndatapts'}));
+for i = 1:length(G1)
+    G = G1(i);
+    GIDs = find(G1==G);
+[G2,ID2] = findgroups(mdlparstbl.nmeshpts);
+parity = splitapply(@(x){x},mdlparstbl.parity,G);
+medRMSE = splitapply(@(x)median(x),mdlparstbl.rmse,G);
+medMAE = splitapply(@(x)median(x),mdlparstbl.mae,G);
+
+    plottype = 'scatter';
+%     switch plottype
+%         case 'hex'
+%             h = hexscatter(ptmp.ytrue,ptmp.ypred,[0 1.5],[0 1.5],'charlbl',charlbl{i},'title',t{i},'cscale','log','cbnds',[1 500]);
+%         case 'scatter'
+            parityplot(ptmp.ytrue,ptmp.ypred,plottype,'charlbl',charlbl{i},'title',t{i},'scatterOpts',struct('MarkerEdgeAlpha',0.1))
+%     end
+
+
+
+
+fig=figure;
+fig.Position = [418.6 194.6 644.8 555.2];
+
+tiledlayout('flow','TileSpacing','compact','Padding','compact')
+nIDs = length(ID3);
+for i = 1:nIDs
+    nexttile
+    ptmp = parity3{i};
+    charlblQ = T;
+    titleQ = F;
+    if charlblQ
+        alphabet = ('a':'z').';
+        chars = num2cell(alphabet(1:nIDs));
+        chars = chars.';
+        charlbl = strcat('(',chars,')');
+    else
+        charlbl = repelem({''},length(ID3));
+    end
+    if titleQ
+        t = ID3;
+    else
+        t = repelem({''},length(ID3));
+    end
+    plottype = 'hex';
+    parityplot(ptmp.ytrue,ptmp.ypred,plottype,'charlbl',charlbl{i},'title',t{i},'scatterOpts',struct('MarkerEdgeAlpha',0.1))
+end
+
+%}
