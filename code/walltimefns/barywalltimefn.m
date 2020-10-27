6da97e3754060e94d0500c6ecf5335464fad9c12
sgbaird <45469701+sgbaird@users.noreply.github.com>
2020-10-27 10:26:29 -0600

    Update barywalltimefn.m

--- code/walltimefns/barywalltimefn.m
+++ code/walltimefns/barywalltimefn.m
@@ -1,4 +1,10 @@
-function walltime = barywalltimefn(ndatapts,npredpts,cores)
+function walltime = barywalltimefn(ndatapts,npredpts,cores,datatype)
+arguments
+   ndatapts(1,1) double
+   npredpts(1,1) double
+   cores(1,1) double
+   datatype char {mustBeMember(datatype,{'brk','kim'})} = 'brk'
+end
 switch ndatapts
     case num2cell(1:1000)
         walltime0 = 5*cores;
@@ -8,10 +14,25 @@ switch ndatapts
         walltime0 = 30*cores;
     case num2cell(10001:50000)
         walltime0 = 90*cores;
+    case num2cell(50001:100000)
+        walltime0 = 300*cores;
     otherwise
         error('too many datapts, update barywalltimefn')
 end
 
+scale = 1;
+walltime1 = scale*npredpts/cores; % scale was ~0.7 for 1000 mesh points, 500 data points, 6 cores
+
+walltime = (walltime0 + walltime1)/cores;
+
+if strcmp(datatype,'kim')
+    walltime = 2*walltime;
+end
+
+end
+
+%% CODE GRAVEYARD
+%{
 % switch npredpts
 %     case num2cell(1:1000)
 %         walltime1 = 100;
@@ -24,10 +45,4 @@ end
 %     otherwise
 %         error('too many predpts, update barywalltimefn')
 % end
-
-scale = 1;
-walltime1 = scale*npredpts/cores; % scale was ~0.7 for 1000 mesh points, 500 data points, 6 cores
-
-walltime = (walltime0 + walltime1)/cores;
-
-end
+%}
