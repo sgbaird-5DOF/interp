64fedf3dfc1e5072a5c23513049ad62f85c20d58
sgbaird <45469701+sgbaird@users.noreply.github.com>
2020-10-27 11:25:26 -0600

    update randOctParityData.m

--- code/randOctParityData.m
+++ code/randOctParityData.m
@@ -4,7 +4,7 @@ clear; close all
 %octochorically sampled octonions
 addpathdir({'var_names.m','writeparfile.m','walltimefns'})
 runtype = 'test'; %'test','full'
-nreps = 5; % number of runs or repetitions
+nreps = 10; % number of runs or repetitions
 
 %make sure the parameters here correspond with the input to "pars" below
 switch runtype
@@ -29,11 +29,11 @@ comment = 'set4';
 % comment = 'idw-test-3pt5deg';
 
 % job submission environment
-env = 'slurm'; %'slurm', 'local'
+env = 'local'; %'slurm', 'local'
 T = true;
 F = false;
 %whether to skip running the jobs and just compile results
-dryrunQ = F;
+dryrunQ = T;
 disp(['env = ' env])
 
 if strcmp(env,'slurm') && dryrunQ
@@ -158,7 +158,9 @@ switch env
             
             if metaQ
                 S = load(fpath);
-                mdlparslist{i} = S;
+                if any(strcmp(S.datatype,datatype))
+                    mdlparslist{i} = S;
+                end
             else
                 loadvars = {'ypred','mdl','mdlpars','interpfn'};
                 S = load(fpath,loadvars{:});
@@ -166,11 +168,22 @@ switch env
                 % delete a previous erroneous data file you generated with the
                 % wrong variable names. Typically shouldn't be an issue
                 % though..
-                [ypredlist{i},mdllist{i},mdlparslist{i},interpfnlist{i}] = ...
+                if any(strcmp(S.mdl.datatype,datatype))
+                    [ypredlist{i},mdllist{i},mdlparslist{i},interpfnlist{i}] = ...
                     deal(S.ypred, S.mdl, S.mdlpars, S.interpfn);
+                end
             end
             %             Slist{i} = S;
         end
+        
+        emptyIDs = cellfun(@isempty,mdlparslist);
+        if metaQ
+            [mdlparslist{emptyIDs}] = deal([]);
+        else
+            [ypredlist{emptyIDs},mdllist{emptyIDs},mdlparslist{emptyIDs},interpfnlist{emptyIDs}] = ...
+            deal([]);
+        end
+            
         
         if ~metaQ
             %concatenate models and parameters
