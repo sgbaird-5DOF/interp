7dea6bfabd029310ddfe92434326448a64638795
sgbaird <45469701+sgbaird@users.noreply.github.com>
2020-10-30 20:58:40 -0600

    scale based on coordinates

--- code/quivplot.m
+++ code/quivplot.m
@@ -3,13 +3,13 @@ arguments
    scl(1,1) double = 1
 end
 % QUIVPLOT  plot three quivers in the x-hat, y-hat, and z-hat directions
-x = [1,0,0]*scl;
-y = [0,1,0]*scl;
-z = [0,0,1]*scl;
+x = [1;0;0]*scl;
+y = [0;1;0]*scl;
+z = [0;0;1]*scl;
 
-w = [0,0,0];
+w = [0;0;0];
 hold on
-quiver3(w,w,w,x,y,z,1,'linewidth',1)
+quiver3(w,w,w,x,y,z,0,'linewidth',1)
 text(x(1),x(2)+0.01,x(3),'x','FontWeight','bold')
 text(y(1),y(2),y(3)+0.01,'y','FontWeight','bold')
 text(z(1)+0.01,z(2),z(3),'z','FontWeight','bold')
