ffaf40f5fe55252d5a62796824c0eff6442a67de
sgbaird <45469701+sgbaird@users.noreply.github.com>
2020-11-03 16:37:55 -0700

    use paperfigure

--- code/multixyplots.m
+++ code/multixyplots.m
@@ -6,13 +6,14 @@ arguments
     ytypes(1,:) = {'rmse','mae'}
     nrows(1,1) double = []
     ncols(1,1) double = []
-    NV.yunits char = 'J/m^2'
+    NV.yunits char = '$J m^{-2}$'
     NV.XScale char {mustBeMember(NV.XScale,{'log','linear'})} = 'log'
     NV.YScale char {mustBeMember(NV.YScale,{'log','linear'})} = 'linear'
     NV.xmin double = []
     NV.ymin double = []
     NV.lgdloc char = 'best'
     NV.charlblQ(1,1) logical = true
+    NV.Interpreter char {mustBeMember(NV.Interpreter,{'latex','tex'})} = 'latex'
 end
 
 if iscell(xytypes{1})
@@ -30,28 +31,26 @@ if ~iscell(ytypes)
     ytypes = {ytypes};
 end
 
-fig = figure;
-switch ntypes
-    case 1
-        fig.Position = [460.2 245 498.4 472.8];
-    case 2
-        if (nrows == 2) && (ncols == 1)
-            fig.Position = [489 90.6 330.4 672.4];
-        end
-end
+paperfigure(nrows,ncols);
+% fig = figure;
+% switch ntypes
+%     case 1
+%         fig.Position = [460.2 245 498.4 472.8];
+%     case 2
+%         if (nrows == 2) && (ncols == 1)
+%             fig.Position = [489 90.6 330.4 672.4];
+%         end
+% end
 
 if ~isempty(nrows) && ~isempty(ncols)
     assert(nrows*ncols >= ntypes,['too many ytypes(' int2str(nrows*ncols) ')/(' int2str(ntypes) ')too few tiles'])
-        tiledlayout(nrows,ncols,'TileSpacing','compact','Padding','compact');
-else
-    tiledlayout('TileSpacing','compact','Padding','compact');
+%         tiledlayout(nrows,ncols,'TileSpacing','compact','Padding','compact');
+% else
+%     tiledlayout('TileSpacing','compact','Padding','compact');
 end
 
 if NV.charlblQ
-    alphabet = ('a':'z').';
-    chars = num2cell(alphabet(1:ntypes));
-    chars = chars.';
-    charlbl = strcat('(',chars,')');
+    charlbl = get_charlbl(ntypes);
 else
     charlbl = repelem({''},ntypes,1);
 end
