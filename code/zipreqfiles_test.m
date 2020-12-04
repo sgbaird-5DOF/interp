b7e7ded474feb23903fdd0e045a9ec79181c8e60
sgbaird <45469701+sgbaird@users.noreply.github.com>
2020-12-04 02:46:52 -0700

    zipreqfiles

---
+++ code/zipreqfiles_test.m
@@ -0,0 +1,5 @@
+%ZIPREQFILES_TEST
+fname = 'eumA2five_test2.m';
+expression = 'interp*';
+zipfname = 'eumA2five_test2_201201';
+[fpathshort,nameExt,plist,fnames] = zipreqfiles(fname,expression,zipfname,'removefname',false);
