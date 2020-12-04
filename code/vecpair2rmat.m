0d9b4bd4f05d74459262ec28d19d91c1547ba490
sgbaird <45469701+sgbaird@users.noreply.github.com>
2020-12-04 02:46:32 -0700

    not sure whether to transpose or not

--- code/vecpair2rmat.m
+++ code/vecpair2rmat.m
@@ -1,18 +1,22 @@
 function R = vecpair2rmat(v1,v2)
+arguments
+   v1(1,3) double
+   v2(1,3) double
+end
 % VECPAIR2RMAT  Compute a (non-unique) rotation matrix to go from v1 to v2.
 %  If v1 == v2 or v1 == -v2 within the given numerical precision, then the
-%  identity matrix or -1*identity matrix is given, respectively.
+%  identity matrix or -1*identity matrix is given, respectively. Passive
+%  rotation matrix
 %--------------------------------------------------------------------------
 % Author: Sterling Baird
 %
 % Date: 2020-07-01
 %
 % Inputs:
-%		v1, v2	===	two vectors to find the rotation matrix to go from
-%							v1->v2
+%  v1, v2 - two vectors to find the rotation matrix to go from v1->v2
 %
 % Outputs:
-%		R			=== Rotation matrix such that R*v1 == v2, and R\v2 == v1
+%  R - Rotation matrix such that R*v1 == v2, and R\v2 == v1
 %
 % References
 %	https://math.stackexchange.com/a/897677
@@ -37,6 +41,10 @@ elseif isOpposite
 	% vectors pointing in opposite directions
 	R = -eye(3);
 end
+
+% if epsijk == -1 %-1 seems to produce consistent results with section 4.2 of 10.1088/0965-0393/23/8/083501
+% R = R.'; %change from active to passive rotation matrix
+% end
 
 end
 %----------------------------vecpair2rmat----------------------------------
