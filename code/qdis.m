f5263216ef86b98ab48b865b33ea336c7d69a54f
sgbaird <45469701+sgbaird@users.noreply.github.com>
2020-12-05 00:45:32 -0700

    tracking inversion, negation, etc.

--- code/qdis.m
+++ code/qdis.m
@@ -1,16 +1,9 @@
-function qd = qdis(q,pgnum,epsijk)
+function [qd,negQ,invQ,disSpair] = qdis(q,pgnum,epsijk)
 arguments
     q(:,4)
     pgnum(1,1) double = 32 %only set up for cubic 2020-12-01
     epsijk(1,1) double = 1
 end
-
-%% load symmetry operators
-%load operators
-symops = load('PGsymops.mat');
-%unpack point group
-qpt = symops.Q{pgnum};
-nsym = size(qpt,1);
 
 %% setup
 npts = size(q,1);
@@ -20,18 +13,26 @@ qd = zeros(npts,4);
 for i = 1:npts
     % get symmetrically equivalent quaternions
     Spairs = get_sympairs(pgnum);
+    
+    % separate symmetry pairs
     S1 = Spairs(:,1:4);
     S2 = Spairs(:,5:8);
-%     S1 = [S1; -S1];
-%     S2 = [S2; -S2];
+    
+    % apply symmetry operators
     nsympairs = size(S1,1);
     qrep = repmat(q(i,:),nsympairs,1);
-    qset = qmult(S1,qmult(qrep,qinv(S2),epsijk),epsijk); %passive convention?
-    qset = [qset; qinv(qset)]; %#ok<AGROW>
-    % disorientation IDs
-%     r = qu2ro(qset,epsijk);
+    qset1 = qmult(S1,qmult(qrep,S2,epsijk),epsijk); %passive convention?
+    qset2 = qmult(S1,qmult(qinv(qrep),S2,epsijk),epsijk);
+    % add inverse as well (should check with Grimmer H. Acta crystallographica section a 1980;36:382-389)
+%     qset = [qset; qinv(qset)]; %#ok<AGROW>
+    qset = [qset1; qset2];
+    
+    %rodrigues vectors
+    %     r = qu2ro(qset,epsijk);
     r = q2rod(qset);
-    isdis = inmisFZ(r,1e-3);
+    
+    % disorientation IDs
+    isdis = inmisFZ(r,1e-6);
     
     % take first non-zero entry
     disID = find(isdis);
@@ -40,6 +41,33 @@ for i = 1:npts
     % assign disorientation
     qd(i,:) = qset(disID,:);
     if qd(i,1) < 0
+        %convention that q0 is positive
         qd(i,:) = -qd(i,:);
+        negQ = true;
+    else
+        negQ = false;
     end
+    
+    if disID > nsympairs
+        invQ = true;
+        disID2 = disID - nsympairs;
+    else
+        invQ = false;
+        disID2 = disID;
+    end
+    disSpair = Spairs(disID2,:);
+    
 end
+
+%% CODE GRAVEYARD
+%{
+%% load symmetry operators
+%load operators
+% symops = load('PGsymops.mat');
+% for point group symmetry names, see PGsymnames.mat
+%unpack point group
+qpt = symops.Q{pgnum};
+
+%     S1 = [S1; -S1];
+%     S2 = [S2; -S2];
+%}
