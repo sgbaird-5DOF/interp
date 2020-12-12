9b67c5a6387abadc40212442804a8d1e10d88910
sgbaird <45469701+sgbaird@users.noreply.github.com>
2020-12-12 16:35:04 -0700

    Update tunnelplot.m

--- code/tunnelplot.m
+++ code/tunnelplot.m
@@ -1,6 +1,6 @@
-function tunnelplot(interpfn,a,b,n)
+function tunnelplot(mdl,a,b,n)
 arguments
-    interpfn = []
+    mdl = []
     a = []
     b = []
     n = []
@@ -14,15 +14,26 @@ end
 % S2str = 'nn1000_gitID-9a0720b_puuID-441cf59f_rohrer-Ni-lamb1000.mat';
 
 % Sstr = 'gpr10000_gitID-9a0720b_puuID-2002d1ea_rohrer-Ni-lamb500m.mat';
-Sstr = 'gpr10000_gitID-9a0720b_puuID-2002d1ea_rohrer-Ni-lamb300m.mat';
+% Sstr = 'gpr10000_gitID-9a0720b_puuID-2002d1ea_rohrer-Ni-lamb300m.mat';
 
-S = load(Sstr);
+% S = load(Sstr);
 % S2 = load(S2str);
 
+nnQ = true;
+nnQ2 = true;
+brkQ = true;
+
 % unpack
-mdl = S.mdl;
-gprMdl = mdl.gprMdl;
-% ppts = mdl.mesh.ppts;
+% mdl = S.mdl;
+if isfield(mdl,'gprMdl')
+    if ~isempty(mdl.gprMdl)
+        gprMdl = mdl.gprMdl;
+    end
+else
+    gprMdl = mdl.cgprMdl;
+end
+% gprMdl = mdl.gprMdl;
+ppts = mdl.mesh.ppts;
 ppts2 = mdl.data.ppts;
 
 %% pairwise distance
@@ -32,13 +43,17 @@ npts2 = size(ppts2,1);
 
 %% max dimension
 [row,col] = ind2sub([npts2,npts2],id);
-a = ppts2(row,:);
-b = ppts2(col,:);
-n = 200;
+% a = ppts2(row,:);
+% b = ppts2(col,:);
+a = [-0.204792525808597,-0.0645669209001381,-0.107955224738106,0.0381947287488522,0.0993759868054296,-0.0225722216770392,0.0189130134087235];
+b = [0.287494800345075,-0.00106803759647395,0.0779976779290583,-0.0258833561524949,-0.127050434093774,-0.00810139755838871,0.0258975652026086];
+n = 300;
 % pts = mdl.mesh.pts;
 pts2 = mdl.data.pts;
-A = pts2(row,:);
-B = pts2(col,:);
+% A = pts2(row,:);
+% B = pts2(col,:);
+A = proj_up(a,mdl.usv);
+B = proj_up(b,mdl.usv);
 
 %% distance calculations/coordinate interpolation
 d1 = get_omega(A,B);
@@ -69,7 +84,7 @@ propList = mdl.mesh.props*scl;
 
 %%
 K = 6;
-[~,nnd,~,~,ids] = get_knn(ppts2,'norm',K,'Y',arcppts);
+[~,nnd,~,~,ids] = get_knn(ppts,'norm',K,'Y',arcppts);
 tprednn = cellfun(@(id) propList(id),n2c(ids),'UniformOutput',false);
 % tprednn = propList(dsearchn(ppts,arcppts));
 
@@ -78,33 +93,52 @@ x = linspace(0,rad2deg(d2),n);
 hold on
 ax = cell(3,1);
 sz2 = zeros(n,K);
-for i = 1:K
-    sz = -rescale(nnd{i},-50,-2);
-%     sz2 = 2*rad2deg(-nnd{i}+2*min(nnd{i}));
-    sz2(:,i) = 2*rad2deg(nnd{i});
-    scatter(x,tprednn{i},sz,sz2(:,i),'o')
-end
-ax{1} = plot(x,tprednn{1},'k--','LineWidth',1);
 
-ax{2}=shadedErrorBar(x,tpred,tsd);
-ax{2}.mainLine.LineWidth = 2.5;
+if nnQ2
+    for i = 1:K
+        sz = -rescale(nnd{i},-50,-2);
+        %     sz2 = 2*rad2deg(-nnd{i}+2*min(nnd{i}));
+        sz2(:,i) = 2*rad2deg(nnd{i});
+        scatter(x,tprednn{i},sz,sz2(:,i),'o')
+    end
+end
+i = 1;
+[lgdax,lgdlbl]=deal({});
+if nnQ
+    ax{i} = plot(x,tprednn{1},'k--','LineWidth',1); %#ok<*UNRCH>
+    lgdlbl = [lgdlbl,'$\overline{AB}$ 1st-NN'];
+    lgdax = [lgdax,ax{i}];
+    i = i+1;
+end
+axtmp=shadedErrorBar(x,tpred,tsd);
+ax{i}=axtmp.mainLine;
+ax{i}.LineWidth = 2.5;
 
 pA = arcpts(:,1:4);
 pB = arcpts(:,5:8);
-brk = GB5DOF_setup(pA,pB,[0 0 1],1);
 
-ax{3} = plot(x,brk,'r-','LineWidth',1);
+lgdax = [lgdax,ax{i}];
+lgdlbl = [lgdlbl,'$\overline{AB}$ GPR'];
+i = i+1;
+if brkQ
+    brk = GB5DOF_setup(pA,pB,[0 0 1],'Ni',1);
+    ax{i} = plot(x,brk,'r-','LineWidth',1);
+    lgdax = [lgdax,ax{i}];
+    lgdlbl = [lgdlbl,'$\overline{AB}$ BRK'];
+end
 
+if nnQ2
+    cb = colorbar;
+    cb.Label.String = 'distance to $\overline{AB}$ $(^\circ)$';
+    cb.Label.Interpreter = 'latex';
+    caxis([min(sz2,[],'all'),max(sz2,[],'all')])
+    colormap jet
+end
 % kstr = num2cell(strcat(string(num2cell(1:K)),'-NN'));
 
-legend([ax{1},ax{2}.mainLine,ax{3}],{'$\overline{AB}$ 1st-NN','$\overline{AB}$ GPR','$\overline{AB}$ BRK'},'Location','northoutside','Interpreter','latex')
+legend(lgdax,lgdlbl,'Location','northoutside','Interpreter','latex')
 
 xlabel('$\overline{AB}(t)$ $(^\circ)$','Interpreter','latex')
 ylabel('$GBE (J m^{-2})$','Interpreter','latex')
-cb = colorbar;
-cb.Label.String = 'distance to $\overline{AB}$ $(^\circ)$';
-cb.Label.Interpreter = 'latex';
-caxis([min(sz2,[],'all'),max(sz2,[],'all')])
-colormap jet
 axis square tight
 end
