04e0c6d892f4809b601c96a75f153f028c3a9458
sgbaird <45469701+sgbaird@users.noreply.github.com>
2020-12-12 16:32:22 -0700

    Update get_alen.m
    check for imaginary part

--- code/get_alen.m
+++ code/get_alen.m
@@ -3,10 +3,14 @@ arguments
     pts double
     pts2 double
 end
-% GET_ALEN  get arclength of sphere (acos of dot product)
+% GET_ALEN  get arclength of sphere (acos of dot product, note that this is half the octonion distance)
 npts = size(pts2,1);
 if size(pts,1) == 1
     pts = repmat(pts,npts,1);
 end
 alen = acos(dot(pts,pts2,2));
+thr = 1e-6;
+maxim = max(abs(imag(alen)));
+assert(maxim <= thr,['max imaginary component alen > ' num2str(thr) ', == ' num2str(maxim)])
+alen = real(alen);
 end
