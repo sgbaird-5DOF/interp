014bf7060613b9a40b8bff15ef68afa7e6bb9c58
sgbaird <45469701+sgbaird@users.noreply.github.com>
2020-12-31 19:24:49 -0700

    turns out mustBeA was introduced in R2020b, so changed to use islogical instead

--- code/mustBeLogical.m
+++ code/mustBeLogical.m
@@ -2,5 +2,8 @@ function mustBeLogical(value)
 % MUSTBELOGICAL  must be a logical value, error otherwise
 %NOTE: do not use "arguments" syntax here since this is a validation fn
 %---------------------HELPER VALIDATION FUNCTION---------------------------
-mustBeA(value,'logical')
+if ~islogical(value)
+    error('value must be of type logical')
+end
+%mustBeA(value,'logical')
 end
