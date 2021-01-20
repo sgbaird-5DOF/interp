47425575e5b196260cbb82b65eedcd65febf4833
sgbaird <45469701+sgbaird@users.noreply.github.com>
2021-01-20 14:53:52 -0700

    Update papertext.m

--- code/papertext.m
+++ code/papertext.m
@@ -3,11 +3,12 @@ arguments
     num(1,1) double
     Interpreter = 'latex'
     NV.xypos(1,2) double = [0.025,0.95]
+    NV.fontsize double = 12
 end
 % PAPERTEXT  do a text() command for the figure labels '(a)', '(b)', etc. for num == 1, 2, etc.
 x = NV.xypos(1);
 y = NV.xypos(2);
 charlbl = get_charlbl();
 
-text(x,y,charlbl{num},'Units','normalized','FontSize',12,'Interpreter',Interpreter)
+text(x,y,charlbl{num},'Units','normalized','FontSize',NV.fontsize,'Interpreter',Interpreter)
 end
