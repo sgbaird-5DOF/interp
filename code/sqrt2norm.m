9fd455e5c0ef463db086c13babd7a60ff91837ef
sgbaird <45469701+sgbaird@users.noreply.github.com>
2021-02-15 12:10:10 -0700

    Update sqrt2norm.m
    whether to warn about norms of octonions

--- code/sqrt2norm.m
+++ code/sqrt2norm.m
@@ -1,8 +1,9 @@
-function pts = sqrt2norm(pts,type)
+function pts = sqrt2norm(pts,type,nv)
 % SQRT2NORM  take a set of octonions and give each row norm == sqrt(2) if (norm == 1) || (norm == sqrt(2))
 arguments
-	pts(:,8) double {mustBeNumeric,mustBeFinite}
-	type char {mustBeMember(type,{'oct','quat'})} = 'oct'
+    pts(:,8) double {mustBeNumeric,mustBeFinite}
+    type char {mustBeMember(type,{'oct','quat'})} = 'oct'
+    nv.warnQ(1,1) logical = true
 end
 %--------------------------------------------------------------------------
 % Author: Sterling Baird
@@ -10,7 +11,7 @@ end
 % Date: 2020-07-27
 %
 % Description: a
-% 
+%
 % Inputs:
 %		a -	a
 %
@@ -26,37 +27,44 @@ end
 % Notes:
 %		*
 %--------------------------------------------------------------------------
-tol = 1e-1;
+tol = 1e-2;
+warnQ = nv.warnQ;
 switch type
-	case 'quat'
-		% if norm(o) == 1 within tolerance, multiply by sqrt(2)
-		if all(abs(vecnorm(pts(:,1:4),2,2)-1/sqrt(2)) < tol) ...
-				&& ...
-				all(abs(vecnorm(pts(:,5:8),2,2)-1/sqrt(2)) < tol)
-			
-			pts(:,1:4) = normr(pts(:,1:4));
-			pts(:,5:8) = normr(pts(:,5:8));
-			
-		elseif all(abs(vecnorm(pts(:,1:4),2,2)-1) > tol) ...
-				&& ...
-				all(abs(vecnorm(pts(:,5:8),2,2)-1) > tol)
-			
-			error(['norm(qA) == ' num2str(norm(pts(1,1:4))) ', ' ...
-				'norm(qB) == ' num2str(norm(pts(1,5:8))) ...
-				'. norm of 1+ quaternions ~= 0.7071 || 1'])
-			
-		end
-	case 'oct'
+    case 'quat'
+        % if norm(o) == 1 within tolerance, multiply by sqrt(2)
+        if all(abs(vecnorm(pts(:,1:4),2,2)-1/sqrt(2)) < tol) ...
+                && ...
+                all(abs(vecnorm(pts(:,5:8),2,2)-1/sqrt(2)) < tol)
+            
+            %fine, no warning
+            
+        elseif all(abs(vecnorm(pts(:,1:4),2,2)-1) >= tol) ...
+                || ...
+                all(abs(vecnorm(pts(:,5:8),2,2)-1) >= tol)
+            
+            if warnQ
+                warning(['norm(qA) == ' num2str(norm(pts(1,1:4))) ', ' ...
+                    'norm(qB) == ' num2str(norm(pts(1,5:8))) ...
+                    '. norm of 1+ quaternions ~= 0.7071 || 1'])
+            end
+        end
+        pts(:,1:4) = normr(pts(:,1:4));
+        pts(:,5:8) = normr(pts(:,5:8));
+        
+    case 'oct'
         ptnm = vecnorm(pts,2,2);
         if ~isempty(find(ptnm == 0, 1))
-%            warning('identity octonion(s) present')
-           ptnm(ptnm == 0) = [];
+            %            warning('identity octonion(s) present')
+            ptnm(ptnm == 0) = [];
         end
-		if all(abs(ptnm - 1) < tol)
-			pts = normr(pts)*sqrt(2);
-		elseif any(abs(ptnm - sqrt(2)) > tol)
-			error('norm of octonions ~= 1 || sqrt(2)')
-		end
+        if all(abs(ptnm - 1) < tol)
+            %fine, no warning
+        elseif any(abs(ptnm - sqrt(2)) >= tol)
+            if warnQ
+                warning('norm of octonions ~= 1 || sqrt(2)')
+            end
+        end
+        pts = normr(pts)*sqrt(2);
 end
 
 end
