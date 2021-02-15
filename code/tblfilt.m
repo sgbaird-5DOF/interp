b34e0a0977555cfd3ce4e126bac7758ee91f72a9
sgbaird <45469701+sgbaird@users.noreply.github.com>
2021-02-15 14:14:16 -0700

    Update tblfilt.m
    display first name of variable that results in no matches

--- code/tblfilt.m
+++ code/tblfilt.m
@@ -1,10 +1,15 @@
 function tbl = tblfilt(tbl,pars)
 % TBLFILT  filter a table based on a struct of parameters (deprecated, see built-in findgroups() and splitapply())
 names = fieldnames(pars);
+checkQ = false;
 for i = 1:length(names)
     name = names{i};
     if ismember(name,tbl.Properties.VariableNames)
         ids = any(tbl.(name) == pars.(name),2);
         tbl = tbl(ids,:);
+        if sum(ids) == 0 && ~checkQ
+            warning([name ' and possibly others resulted in no matches'])
+            checkQ = true;
+        end
     end
 end
