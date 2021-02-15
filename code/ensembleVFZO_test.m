f2da8725a9feadf4ca5a6dcfaa2c20a5ec9bca3e
sgbaird <45469701+sgbaird@users.noreply.github.com>
2021-02-15 12:08:11 -0700

    Update ensembleVFZO_test.m
    turn into function

--- code/ensembleVFZO_test.m
+++ code/ensembleVFZO_test.m
@@ -1,24 +1,25 @@
-%ENSEMBLEVFZO_TEST  
-addpathdir({'cu2qu.m','get_uuid.m','gmat2q.m'})
-n = 1000;
-n2 = 1000;
+function [ypred,ytrue,ypredlist,interpfnlist,mdllist,mdlparslist] = ensembleVFZO_test(n,n2,K,method,epsijk)
+arguments
+    n(1,1) double = 1000
+    n2(1,1) double = 1000
+    K(1,1) double = 10
+    method char = 'gpr'
+    epsijk(1,1) double = 1
+end
+%ENSEMBLEVFZO_TEST  run a K-component ensemble for n input and n2 prediction GBs using the specified method
+% addpathdir({'cu2qu.m','get_uuid.m','gmat2q.m'}) %use addpath(genpath('.')) in a top-level folder instead
 [five,qm,nA] = get_five(n); %input
 [five2,qm2,nA2] = get_five(n2); %prediction
-epsijk = 1;
 y = GB5DOF_setup([],qm,nA,'Ni',epsijk);
 ytrue = GB5DOF_setup([],qm2,nA2,'Ni',epsijk);
-K = 1;
-[ypred,ypredlist,interpfnlist,mdllist,mdlparslist] = ensembleVFZO(qm,nA,y,qm2,nA2,K,'gpr');
+[ypred,ypredlist,interpfnlist,mdllist,mdlparslist] = ensembleVFZO(qm,nA,y,qm2,nA2,K,method);
 
 errmetrics = get_errmetrics(ypred,ytrue,'dispQ',true);
-
-paperfigure();
-parityplot(ytrue,ypred);
-
-% save('ensemble-interp.mat')
 
 %% CODE GRAVEYARD
 %{
 % y = GB5DOF_setup(five);
 % ytrue = GB5DOF_setup(five2);
+% n = 1000;
+% n2 = 1000;
 %}
