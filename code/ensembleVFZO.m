ae80c0b0dedd7429236297ac21862dc4810f2df2
sgbaird <45469701+sgbaird@users.noreply.github.com>
2021-02-15 12:08:02 -0700

    Update ensembleVFZO.m
    mostly dispQ and input argument changes

--- code/ensembleVFZO.m
+++ code/ensembleVFZO.m
@@ -1,4 +1,4 @@
-function [ypred,ypredlist,interpfnlist,mdllist,mdlparslist] = ensembleVFZO(qm,nA,y,qm2,nA2,K,method,NV)
+function [ypred,ypredlist,interpfnlist,mdllist,mdlparslist] = ensembleVFZO(qm,nA,y,qm2,nA2,K,method,epsijk,NV)
 arguments
     qm %input misorientation quaternions
     nA %input BP normals
@@ -7,6 +7,7 @@ arguments
     nA2 %query BP normals
     K(1,1) double = 1 % number of ensembles
     method char {mustBeMember(method,{'gpr','sphgpr','pbary','sphbary','idw','nn','avg'})} = 'gpr'
+    epsijk(1,1) double = 1
     NV.pgnum(1,1) double = 32 %m-3m (i.e. m\overbar{3}m) FCC symmetry default
     NV.databary = [] %for use with bary methods
     NV.facetIDs = [] %for use with bary methods
@@ -18,15 +19,24 @@ arguments
     NV.uuid(1,8) char = get_uuid() %unique ID associated with this interpolation run
     NV.o = [] %input octonions, specify these or qm/nA pairs
     NV.o2 = [] %query octonions, specify these or qm2/nA2 pairs
+    NV.sigma(1,1) double = 0 %synthetic input noise
+    NV.dispQ(1,1) logical = false
+    NV.KdispQ(1,1) logical = true
 end
+% ENSEMBLEVFZO  take the ensemble average of K VFZs for same set of GBs
+KdispQ = NV.KdispQ;
 [ypredlist,interpfnlist,mdllist,mdlparslist] = deal(cell(K,1)); %initialize
 for k = 1:K
+    if KdispQ
+        disp([int2str(k) '-th ensemble component (ensembleVFZO.m)'])
+    end
     %random reference octonion
     NV.oref = get_ocubo();
-    NVpairs = namedargs2cell(NV);
+    NVsub = rmfield(NV,{'KdispQ'});
+    NVpairs = namedargs2cell(NVsub);
     %interpolate
     [ypredlist{k},interpfnlist{k},mdllist{k},mdlparslist{k}] = ...
-        interp5DOF(qm,nA,y,qm2,nA2,method,NVpairs{:});
+        interp5DOF(qm,nA,y,qm2,nA2,method,epsijk,NVpairs{:});
 end
 %concatenate
 ypredtmp = [ypredlist{:}];
