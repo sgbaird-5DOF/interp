fe2ec7e6f1c09acd7747afd7133feb8b535b0cce
sgbaird <45469701+sgbaird@users.noreply.github.com>
2021-02-15 12:08:17 -0700

    Update get_walltimefn.m

--- code/get_walltimefn.m
+++ code/get_walltimefn.m
@@ -1,18 +1,19 @@
-function walltimefn = get_walltimefn(ndatapts,npredpts,method,cores,datatype) %#ok<*INUSD,*INUSL>
+function walltimefn = get_walltimefn(ndatapts,npredpts,method,cores,datatype,K) %#ok<*INUSD,*INUSL>
 % GET_WALLTIMEFN  get a function handle for computing interp5DOF SLURM walltimes (e.g. 'gpr')
 arguments
     ndatapts(1,1) double
     npredpts(1,1) double
-    method char {mustBeMember(method,{'sphgpr','gpr','pbary','sphbary','idw','nn','avg'})}
+    method char {mustBeMember(method,{'sphgpr','gpr','pbary','sphbary','idw','nn','avg','egprm','egpr','gprm','gprmix'})}
     cores(1,1) double
     datatype char {mustBeMember(datatype,{'brk','kim','rohrer-Ni',...
         'rohrer-test','rohrer-brk-test','olmsted-Ni'})} = 'brk'
+    K(1,1) double = 1
 end
 walltimeBuffer = 5;
 
 switch method
-    case {'sphgpr','gpr'}
-        walltimefn = gprwalltimefn(ndatapts,cores) + walltimeBuffer;
+    case {'sphgpr','gpr','egprm','egpr','gprm','gprmix'}
+        walltimefn = K*gprwalltimefn(ndatapts,cores) + walltimeBuffer;
     case {'pbary','sphbary'}
         walltimefn = barywalltimefn(ndatapts,npredpts,cores,datatype) + walltimeBuffer;
     case {'idw'}
