fdd3399ff645f9d882e6fbd52213e17c1022acba
sgbaird <45469701+sgbaird@users.noreply.github.com>
2021-02-15 12:45:32 -0700

    auto-update egprmSubmit.m

--- code/egprmSubmit.m
+++ code/egprmSubmit.m
@@ -2,8 +2,8 @@
 clear; close all
 
 %make sure this file is committed and pushed (WARNING: any other unpushed commits will also be pushed)
-!git commit -m "update egprmSubmit.m" egprmSubmit.m
-!git push egprmSubmit.m
+system('git commit egprmSubmit.m -m "auto-update egprmSubmit.m"');
+system('git push')
 
 T = true;
 F = false;
