001861d289209e28aa2c37f54df7e4d3554bbc1d
sgbaird <45469701+sgbaird@users.noreply.github.com>
2021-02-15 17:52:05 -0700

    Update interp5DOF.m
    ran into an error with fitrgp even with only 100 GBs

--- code/interp5DOF.m
+++ code/interp5DOF.m
@@ -429,7 +429,8 @@ switch method
                 KernelFunction = 'squaredexponential'; %only one implemented as of 2021-02-15
             end
             kfn = @(XN,XM,theta) ensembleVFZOcov(XN,XM,theta,usv,'K',covK,'KernelFunction',KernelFunction);
-            theta0 = [mean(std(X)), std(y)/sqrt(2)]; %initial length scale and noise, fitrgp defaults for 'squaredexponential'
+%             theta0 = [mean(std(X)), std(y)/sqrt(2)]; %initial length scale and noise, fitrgp defaults for 'squaredexponential'
+            theta0 = [rad2deg(10/2), std(y)/sqrt(2)]; %initial length scale and noise
             gprappend = {'KernelFunction',kfn,'KernelParameters',theta0};
         else
             gprappend = [];
