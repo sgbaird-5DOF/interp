1e4f584069edbb2d122b35c0e10744a77d12006a
sgbaird <45469701+sgbaird@users.noreply.github.com>
2021-03-06 16:11:30 -0700

    auto-update egprmSubmit.m

--- code/egprmSubmit.m
+++ code/egprmSubmit.m
@@ -24,14 +24,14 @@ metaQ = T; %whether to load full model or only meta-data at end
 %for cells and strings, wrap in an outer cell
 
 %comment (no spaces, used in filename)
-comment = 'brk';
+comment = 'tmvn-runtime';
 % list of comments used so far:
-% 'brk', 'kim', 'kim-brk'
+% 'brk', 'kim', 'kim-brk', 'tmvn-runtime'
 
 switch runtype
     case 'test'
         ninputpts = 1000; %ceil(58604*0.8); %17176; %floor(58604*0.2); %56442; %floor(67886*0.8); %floor(264276*.8); %17176; %1893*2; %[2366]; %[1893*1]; % 5000 10000 20000 50000];
-        npredpts = 1000:1000:10000; %floor(58604*0.2); %58604-17176; %ceil(58604*0.8); %11443; %floor(67886*0.2); %ceil(264276*0.2); %67886-17176; %67886-1893*2; %65520; %473*1;
+        npredpts = 500:500:3000; %floor(58604*0.2); %58604-17176; %ceil(58604*0.8); %11443; %floor(67886*0.2); %ceil(264276*0.2); %67886-17176; %67886-1893*2; %65520; %473*1;
         datatype = {'brk'}; % 'brk', 'kim', 'rohrer-Ni', 'rohrer-test', 'rohrer-brk-test', 'olmsted-Ni'
         pgnum = 32; %m-3m (i.e. m\overbar{3}m) FCC symmetry default for e.g. Ni
         sig = [0]; %J/m^2, standard deviation, added to "y"
