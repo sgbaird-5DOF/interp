3b8a9c9572e5d2527276e763bae66e08ffc826a0
sgbaird <45469701+sgbaird@users.noreply.github.com>
2021-03-16 01:18:11 -0600

    move files to egprm and posterior-sampling

--- code/kmix.m
+++
@@ -1,8 +0,0 @@
-function kfn = kmix(kfntmp,kfntmp2)
-arguments
-    kfntmp
-    kfntmp2
-end
-% KMIX  sigmoid-based covariance matrix mixture function
-kfn = @(X1,X2,A) A.*kfntmp(X1,X2)+(1-A).*kfntmp2(X1,X2);
-end
