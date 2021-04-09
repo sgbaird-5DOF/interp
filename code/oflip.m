fa35f5857035974d1f11e82f48afad9dff117c7c
sgbaird <sterling.baird@icloud.com>
2021-04-09 15:57:49 -0600

    Create oflip.m
    take inverse of both quaternions

---
+++ code/oflip.m
@@ -0,0 +1,7 @@
+function o = oflip(o)
+arguments
+   o(:,8) double = get_ocubo(2,'random',[],20)
+end
+% oflip  octonion flip, as in flip the rotation convention (passive, active)
+o = [qinv(o(:,1:4)) qinv(o(:,5:8))];
+end
