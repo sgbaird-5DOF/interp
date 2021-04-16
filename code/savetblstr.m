f25803d895388eda2ad43af0f740296acf35d7b1
sgbaird <sterling.baird@icloud.com>
2021-04-16 14:33:08 -0600

    latexTable and savetblstr

---
+++ code/savetblstr.m
@@ -0,0 +1,21 @@
+function tblstr = savetblstr(tbl,savename,savefolder,caption,nv)
+arguments
+    tbl
+    savename = 'tblstr-test'
+    savefolder = '.'
+    caption = '...'
+    nv.printQ(1,1) logical = true
+end
+%SAVETBLSTR  compile a \begin{table}...\end{table} string and save to .tex file
+printQ = nv.printQ;
+tblstr = latexTable(struct('data',tbl,'tableLabel',savename,'tableCaption',caption,'booktabs',true));
+tblstr = strjoin(tblstr,'\n');
+savepath = fullfile(savefolder,[savename,'.tex']);
+f = fopen(savepath,'w');
+fprintf(f,'%s',tblstr);
+tblstr = fileread(savepath);
+if printQ
+    disp(tblstr);
+end
+fclose(f);
+end
