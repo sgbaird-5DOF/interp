e4ecdf0dd8200285ec4bea3fd74797699f26b8fd
sgbaird <sterling.baird@icloud.com>
2021-07-02 09:59:51 -0600

    Update gprmix_test.m

--- code/gprmix_test.m
+++ code/gprmix_test.m
@@ -1,6 +1,6 @@
 function [ypred,ysd,ytrue,ci,covmat,kfntmp,kfntmp2,mdl,gprMdl2] = gprmix_test(fname)
 arguments
-   fname = 'gpr46883_gitID-b473165_puuID-50ffdcf6_kim-rng11.mat' %gpr46883_gitID-9818307_puuID-e5d12412_kim-rng11.mat
+   fname = 'gpr46883_gitID-5e6bd14_puuID-2e34b447_kim-paper-data3.mat' %'gpr46883_gitID-b473165_puuID-50ffdcf6_kim-rng11.mat' %gpr46883_gitID-9818307_puuID-e5d12412_kim-rng11.mat
 end
 % GPRMIX_TEST  Perform sigmoid-based mixture with model trained on subset of low property values.
 %% setup
