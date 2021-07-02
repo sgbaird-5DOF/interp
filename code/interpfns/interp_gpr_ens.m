e56a77f9cf09a58e47307744d3229e93fec4816d
sgbaird <sterling.baird@icloud.com>
2021-07-02 10:00:19 -0600

    Create interp_gpr_ens.m

---
+++ code/interpfns/interp_gpr_ens.m
@@ -0,0 +1,20 @@
+function [ypred,ypredlist] = interp_gpr_ens(gprMdls,qm2,nA2,projtol,usv,zeroQ)
+% INTERP_GPR  interpolate using Gaussian Process Regression model (gprMdl) and predict()
+pts2 = get_pts(qm2,nA2);
+% ppts2 = get_ppts(pts2,projtol,usv,zeroQ);
+nmdls = length(gprMdls);
+ypredlist = cell(1,nmdls);
+for i = 1:nmdls
+    gprMdl = gprMdls{i};
+    ypredlist{i} = predict(gprMdl,pts2);
+end
+ypred = mean([ypredlist{:}],2);
+
+end
+
+%% CODE GRAVEYARD
+%{
+%command to do interpolation
+mdlcmd = @(gprMdl,ppts2) predict(gprMdl,ppts2);
+interpfn = @(qm2,nA2) mdlcmd(compact(gprMdl),get_ppts(qm2,nA2));
+%}
