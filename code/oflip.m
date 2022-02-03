6cafa50d8eae35a08038c50bec107fc77d0bf23a
sgbaird <sterling.baird@icloud.com>
2022-02-03 15:07:55 -0700

    commentary

--- code/oflip.m
+++ code/oflip.m
@@ -2,6 +2,6 @@ function o = oflip(o)
 arguments
    o(:,8) double = get_ocubo(2,'random',[],20)
 end
-% oflip  octonion flip, as in flip the rotation convention (passive, active)
+% oflip  octonion flip, as in flip the rotation convention
 o = [qinv(o(:,1:4)) qinv(o(:,5:8))];
 end
