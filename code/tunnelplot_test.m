eddb1605ad2688c04bbdcfd2e6980823fc46e60c
sgbaird <sterling.baird@icloud.com>
2022-03-09 22:19:33 -0700

    additional test cases

--- code/tunnelplot_test.m
+++ code/tunnelplot_test.m
@@ -78,6 +78,16 @@ switch testnum
             tunnelplot(mdllist,A,B,n,'nnQ',false,'nnQ2',false,...
             'tpredlist',tpredlist,'tsdlist',tsdlist,'propList',propList,...
             'methodlist',methodlist,'extend',extend,'lgdloc','south');
+    case 3
+        npts = 100;
+        qm = get_cubo(npts); nA = normr(rand(npts,3)); %random (qm,nA) pairs
+        propList = 1:npts; %property values
+        qm2 = get_cubo(npts); nA2 = normr(rand(npts,3)); %random (qm,nA) pairs
+        method = 'gpr'; %interpolation method
+        [propOut,interpfn,mdl,mdlpars] = interp5DOF(qm,nA,propList,qm2,nA2,method);
+    case 4
+        load("SymmetrizedKernelTest_TMS.mat")
+        tunnelplot(mdllist2, A, B)
 end
 
 %% CODE GRAVEYARD
