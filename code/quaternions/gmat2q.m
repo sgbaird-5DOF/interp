76adbcb51c0ea055721176a1f20a31150f70c3cf
sgbaird <sterling.baird@icloud.com>
2022-05-12 17:26:13 -0600

    generalize to handle Homer rotation matrices

--- code/quaternions/gmat2q.m
+++ code/quaternions/gmat2q.m
@@ -15,10 +15,17 @@ end
 % Outputs:
 %   q - An npts-by-4 array of equiavalent quaternions.
 %
-% [1] Method from https://doi.org/10.1007/978-3-319-93188-3_5
+% CURRENT METHOD
+% Shepperd's Method (as described in
+% https://doi.org/10.1007/978-3-319-93188-3_5). Works correctly for both
+% skew-symmetric and symmetric (e.g. rotations by pi) rotation matrices.
+% 
+% OLD METHOD
+% https://doi.org/10.1007/978-3-319-93188-3_5 (Only works for
+% skew-symmetric rotation matrices)
 %
-% OLD METHOD (4/1/2013):
-% [1] Code adapted from http://en.wikipedia.org/wiki/Rotation_matrix#Quaternion
+% OLD OLD METHOD (4/1/2013):
+% Code adapted from http://en.wikipedia.org/wiki/Rotation_matrix#Quaternion
 % This algorithm only works when the rotation matrix is stricly skew
 % symmetric, in the case that it is symmetric the difference of the off
 % diagonal terms is zero so the sign function returns zero. I need to get a
@@ -31,64 +38,109 @@ npts = size(g,3);
 q = zeros(npts,4);
 
 %---compute transformation---%
+% compute the traces
+t = g(1,1,:)+g(2,2,:)+g(3,3,:);
 
-%q0
-t = g(1,1,:)+g(2,2,:)+g(3,3,:); %traces
-t = t(:);
-isPos = t > 0; 
-q(isPos,1) = 0.5*(sqrt(1+t(isPos)));
-q(~isPos,1) = 0.5*(sqrt(...
-    squeeze((g(3,2,~isPos)-g(2,3,~isPos)).^2+(g(1,3,~isPos)-g(3,1,~isPos)).^2+(g(2,1,~isPos)-g(1,2,~isPos)).^2)./...
-    squeeze(3-t(~isPos))));
+% figure out which term is largest (this will determine which case to use)
+[~,id] = max([t,g(1,1,:),g(2,2,:),g(3,3,:)],[],2);
 
-%q1
-t = g(1,1,:)-g(2,2,:)-g(3,3,:);
-t = t(:);
-isPos = t > 0; 
-q(isPos,2) = 0.5*(sqrt(1+t(isPos)));
-q(~isPos,2) = 0.5*(sqrt(...
-    squeeze((g(3,2,~isPos)-g(2,3,~isPos)).^2+(g(1,2,~isPos)+g(2,1,~isPos)).^2+(g(3,1,~isPos)+g(1,3,~isPos)).^2)./...
-    squeeze(3-t(~isPos))));
+% case 1
+id1 = id == 1;
+val = sqrt(1+t(:,:,id1));
+q(id1,1) = 0.5*squeeze(val);
+q(id1,2) = 0.5*squeeze(( g(3,2,id1) - g(2,3,id1) ) ./ val);
+q(id1,3) = 0.5*squeeze(( g(1,3,id1) - g(3,1,id1) ) ./ val);
+q(id1,4) = 0.5*squeeze(( g(2,1,id1) - g(1,2,id1) ) ./ val);
 
-%q2
-t = -g(1,1,:)+g(2,2,:)-g(3,3,:);
-t = t(:);
-isPos = t > 0; 
-q(isPos,3) = 0.5*(sqrt(1+t(isPos)));
-q(~isPos,3) = 0.5*(sqrt(...
-    squeeze((g(1,3,~isPos)-g(3,1,~isPos)).^2+(g(1,2,~isPos)+g(2,1,~isPos)).^2+(g(2,3,~isPos)+g(3,2,~isPos)).^2)./...
-    squeeze(3-t(~isPos))));
+% case 2
+id2 = id == 2;
+val = sqrt(1+g(1,1,id2)-g(2,2,id2)-g(3,3,id2));
+q(id2,1) = 0.5*squeeze(( g(3,2,id2) - g(2,3,id2) ) ./ val);
+q(id2,2) = 0.5*squeeze(val);
+q(id2,3) = 0.5*squeeze(( g(1,2,id2) + g(2,1,id2) ) ./ val);
+q(id2,4) = 0.5*squeeze(( g(3,1,id2) + g(1,3,id2) ) ./ val);
 
-%q3
-t = -g(1,1,:)-g(2,2,:)+g(3,3,:);
-t = t(:);
-isPos = t > 0; 
-q(isPos,4) = 0.5*(sqrt(1+t(isPos)));
-q(~isPos,4) = 0.5*(sqrt(...
-    squeeze((g(2,1,~isPos)-g(1,2,~isPos)).^2+(g(3,1,~isPos)+g(1,3,~isPos)).^2+(g(3,2,~isPos)+g(2,3,~isPos)).^2)./...
-    squeeze(3-t(~isPos))));
+% case 3
+id3 = id == 3;
+val = sqrt(1-g(1,1,id3)+g(2,2,id3)-g(3,3,id3));
+q(id3,1) = 0.5*squeeze(( g(1,3,id3) - g(3,1,id3) ) ./ val);
+q(id3,2) = 0.5*squeeze(( g(1,2,id3) + g(2,1,id3) ) ./ val);
+q(id3,3) = 0.5*squeeze(val);
+q(id3,4) = 0.5*squeeze(( g(2,3,id3) + g(3,2,id3) ) ./ val);
 
-%signs
-q(:,2) = copysign(q(:,2), squeeze(g(3,2,:)-g(2,3,:)));
-q(:,3) = copysign(q(:,3), squeeze(g(1,3,:)-g(3,1,:)));
-q(:,4) = copysign(q(:,4), squeeze(g(2,1,:)-g(1,2,:)));
+% case 4
+id4 = id == 4;
+val = sqrt(1-g(1,1,id4)-g(2,2,id4)+g(3,3,id4));
+q(id4,1) = 0.5*squeeze(( g(2,1,id4) - g(1,2,id4) ) ./ val);
+q(id4,2) = 0.5*squeeze(( g(3,1,id4) + g(1,3,id4) ) ./ val);
+q(id4,3) = 0.5*squeeze(( g(3,2,id4) + g(2,3,id4) ) ./ val);
+q(id4,4) = 0.5*squeeze(val);
 
+% choose overall sign that makes q0 positive
+idChangeSign = q(:,1) < 0;
+q(idChangeSign,:) = -q(idChangeSign,:);
+
+% %% Old Method
 % %---compute transformation---%
+% 
+% %q0
 % t = g(1,1,:)+g(2,2,:)+g(3,3,:); %traces
 % t = t(:);
-% r = sqrt(1+t);
-% q(:,1) = 0.5*r;
-% q(:,2) = copysign(0.5*sqrt(1+g(1,1,:)-g(2,2,:)-g(3,3,:)), g(3,2,:)-g(2,3,:));
-% q(:,3) = copysign(0.5*sqrt(1-g(1,1,:)+g(2,2,:)-g(3,3,:)), g(1,3,:)-g(3,1,:));
-% q(:,4) = copysign(0.5*sqrt(1-g(1,1,:)-g(2,2,:)+g(3,3,:)), g(2,1,:)-g(1,2,:));
-
-end
-
-function x = copysign(x,y)
-
-sgn = ones(size(y));
-sgn(y < 0) = -1;
-
-x = sgn.*x; % note we don't need abs(x) because the equations above guarantee x >= 0 to begin with
-
-end
+% isPos = t > 0; 
+% q(isPos,1) = 0.5*(sqrt(1+t(isPos)));
+% q(~isPos,1) = 0.5*(sqrt(...
+%     squeeze((g(3,2,~isPos)-g(2,3,~isPos)).^2+(g(1,3,~isPos)-g(3,1,~isPos)).^2+(g(2,1,~isPos)-g(1,2,~isPos)).^2)./...
+%     squeeze(3-t(~isPos))));
+% 
+% %q1
+% t = g(1,1,:)-g(2,2,:)-g(3,3,:);
+% t = t(:);
+% isPos = t > 0; 
+% q(isPos,2) = 0.5*(sqrt(1+t(isPos)));
+% q(~isPos,2) = 0.5*(sqrt(...
+%     squeeze((g(3,2,~isPos)-g(2,3,~isPos)).^2+(g(1,2,~isPos)+g(2,1,~isPos)).^2+(g(3,1,~isPos)+g(1,3,~isPos)).^2)./...
+%     squeeze(3-t(~isPos))));
+% 
+% %q2
+% t = -g(1,1,:)+g(2,2,:)-g(3,3,:);
+% t = t(:);
+% isPos = t > 0; 
+% q(isPos,3) = 0.5*(sqrt(1+t(isPos)));
+% q(~isPos,3) = 0.5*(sqrt(...
+%     squeeze((g(1,3,~isPos)-g(3,1,~isPos)).^2+(g(1,2,~isPos)+g(2,1,~isPos)).^2+(g(2,3,~isPos)+g(3,2,~isPos)).^2)./...
+%     squeeze(3-t(~isPos))));
+% 
+% %q3
+% t = -g(1,1,:)-g(2,2,:)+g(3,3,:);
+% t = t(:);
+% isPos = t > 0; 
+% q(isPos,4) = 0.5*(sqrt(1+t(isPos)));
+% q(~isPos,4) = 0.5*(sqrt(...
+%     squeeze((g(2,1,~isPos)-g(1,2,~isPos)).^2+(g(3,1,~isPos)+g(1,3,~isPos)).^2+(g(3,2,~isPos)+g(2,3,~isPos)).^2)./...
+%     squeeze(3-t(~isPos))));
+% 
+% %signs
+% q(:,2) = copysign(q(:,2), squeeze(g(3,2,:)-g(2,3,:)));
+% q(:,3) = copysign(q(:,3), squeeze(g(1,3,:)-g(3,1,:)));
+% q(:,4) = copysign(q(:,4), squeeze(g(2,1,:)-g(1,2,:)));
+% 
+% % %% Old Old Method
+% % %---compute transformation---%
+% % t = g(1,1,:)+g(2,2,:)+g(3,3,:); %traces
+% % t = t(:);
+% % r = sqrt(1+t);
+% % q(:,1) = 0.5*r;
+% % q(:,2) = copysign(0.5*sqrt(1+g(1,1,:)-g(2,2,:)-g(3,3,:)), g(3,2,:)-g(2,3,:));
+% % q(:,3) = copysign(0.5*sqrt(1-g(1,1,:)+g(2,2,:)-g(3,3,:)), g(1,3,:)-g(3,1,:));
+% % q(:,4) = copysign(0.5*sqrt(1-g(1,1,:)-g(2,2,:)+g(3,3,:)), g(2,1,:)-g(1,2,:));
+% 
+% end
+% 
+% function x = copysign(x,y)
+% 
+% sgn = ones(size(y));
+% sgn(y < 0) = -1;
+% 
+% x = sgn.*x; % note we don't need abs(x) because the equations above guarantee x >= 0 to begin with
+% 
+% end
